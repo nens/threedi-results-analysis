bbd8937f4baba536b499059a2695792a530c5da6
Jackie <Jackie.Leng@nelen-schuurmans.nl>
2016-05-06 10:31:52 +0200

    Put the make_pump_layer function in the appropiate module

--- commands/toolbox_tools/stap 5 - Resultaten nabewerken/generate_netcdf_layer.py
+++ commands/toolbox_tools/stap 5 - Resultaten nabewerken/generate_netcdf_layer.py
@@ -1,90 +1,12 @@
-"""This script calculates statistics on the selected layer for structures and
-outputs it to csv.
+"""This script generates node and line layers from netCDF.
 """
-from qgis.core import (
-    QgsVectorLayer, QgsField, QgsFeature, QgsGeometry, QgsPoint,
-    QgsMapLayerRegistry
-    )
-from PyQt4.QtCore import QVariant
+from qgis.core import QgsMapLayerRegistry
 
 from ThreeDiToolbox.utils.user_messages import pop_up_info
 from ThreeDiToolbox.views.tool_dialog import ToolDialogWidget
 from ThreeDiToolbox.commands.base.custom_command import CustomCommandBase
-from ThreeDiToolbox.utils.layer_from_netCDF import make_flowline_layer, make_node_layer
-from ThreeDiToolbox.datasource.netcdf import get_node_mapping
-
-
-def make_pumpline_layer(ds):
-    """Make a memory layer that contains all pumplines.
-
-    Args:
-        ds: netCDF Dataset
-    """
-    # Get relevant netCDF.Variables
-    projection = ds.variables['projected_coordinate_system']
-    epsg = projection.epsg  # = 28992
-    # Pumpline connections (2, jap1d):
-    pumpline = ds.variables['pump_mapping']
-    # FlowElem centers:
-    flowelem_xcc = ds.variables['FlowElem_xcc']  # in meters
-    flowelem_ycc = ds.variables['FlowElem_ycc']  # in meters
-
-    # -1 probably because of fortran indexing
-    pumpline_p1 = pumpline[0, :].astype(int)  # - 1
-    pumpline_p2 = pumpline[1, :].astype(int)  # - 1
-
-    # TODO: Not very efficient, but this mapping is not needed anyway in the
-    # future, so who cares?
-    node_mapping = get_node_mapping(ds)
-    for i in range(pumpline.shape[1]):
-        # Note: there is no need to subtract 1 from the index because the
-        # node_mapping already does this for you
-        pumpline_p1[i] = node_mapping.get(pumpline_p1[i], 0)
-        pumpline_p2[i] = node_mapping.get(pumpline_p2[i], 0)
-
-    # Point 1 of the connection
-    x_p1 = flowelem_xcc[:][pumpline_p1]
-    y_p1 = flowelem_ycc[:][pumpline_p1]
-
-    # Point 2 of the connection
-    x_p2 = flowelem_xcc[:][pumpline_p2]
-    y_p2 = flowelem_ycc[:][pumpline_p2]
-
-    # create layer
-    # "Point?crs=epsg:4326&field=id:integer&field=name:string(20)&index=yes"
-    uri = "LineString?crs=epsg:{}&index=yes".format(
-        epsg)
-    vl = QgsVectorLayer(uri, "pumplines", "memory")
-    pr = vl.dataProvider()
-
-    # add fields
-    pr.addAttributes([
-        # This is the flowline index in Python (0-based indexing)
-        # Important: this differs from the feature id which is flowline idx+1!!
-        QgsField("pumpline_idx", QVariant.Int),
-        ])
-    vl.updateFields()  # tell the vector layer to fetch changes from the provider
-
-    # add features
-    features = []
-    number_of_pumplines = pumpline.shape[1]
-    for i in range(number_of_pumplines):
-        fet = QgsFeature()
-
-        p1 = QgsPoint(x_p1[i], y_p1[i])
-        p2 = QgsPoint(x_p2[i], y_p2[i])
-
-        fet.setGeometry(QgsGeometry.fromPolyline([p1, p2]))
-        fet.setAttributes([i])
-        features.append(fet)
-    pr.addFeatures(features)
-
-    # update layer's extent when new features have been added
-    # because change of extent in provider is not propagated to the layer
-    vl.updateExtents()
-
-    # add the layer
-    QgsMapLayerRegistry.instance().addMapLayers([vl])
+from ThreeDiToolbox.utils.layer_from_netCDF import (
+    make_flowline_layer, make_node_layer, make_pumpline_layer)
 
 
 class CustomCommand(CustomCommandBase):
@@ -115,16 +37,17 @@ class CustomCommand(CustomCommandBase):
             return
         nds = self.datasource.datasource()  # the netcdf datasource
 
-        vlayer = make_flowline_layer(nds)
-        # add the layer
-        QgsMapLayerRegistry.instance().addMapLayers([vlayer])
+        flowline_layer = make_flowline_layer(nds)
+        node_layer = make_node_layer(nds)
 
-        vlayer = make_node_layer(nds)
-        # add the layer
-        QgsMapLayerRegistry.instance().addMapLayers([vlayer])
+        vlayers = [flowline_layer, node_layer]
 
         try:
-            make_pumpline_layer(nds.ds)
+            pump_layer = make_pumpline_layer(nds)
+            vlayers.append(pump_layer)
         except:
             print("Pumps are still in development")
             pass
+
+        # add the layers
+        QgsMapLayerRegistry.instance().addMapLayers(vlayers)
