dcd64c5ed748a498574506306904a6ada7f61d06
bastiaan-roos <bastiaan.roos@nelen-schuurmans.nl>
2016-07-24 10:52:53 +0200

    sideview bugfix in length, refactor map visualisation and
    for sideview:
    - work around for getting the correct lengths of lines for graphs
    - refactor map visualisation into separate class
    - add points add selected connection nodes
    - add marker which indicates location on hover of sideview

--- views/sideview.py
+++ views/sideview.py
@@ -17,7 +17,7 @@ from qgis.core import QgsPoint, QgsRectangle, QgsCoordinateTransform, \
 from qgis.gui import QgsRubberBand, QgsVertexMarker, QgsMapTool
 from collections import Counter
 
-from ..datasource.spatialite import get_object_type, layer_qh_type_mapping
+from ..datasource.result_spatialite import get_object_type, layer_qh_type_mapping
 from ..models.graph import LocationTimeseriesModel
 from ..utils.user_messages import log, statusbar_message
 
@@ -83,7 +83,7 @@ class SideViewPlotWidget(pg.PlotWidget):
     """Side view plot element"""
 
     profile_route_updated = pyqtSignal()
-
+    profile_hovered = pyqtSignal(float)
 
     def __init__(self, parent=None, nr=0, line_layer=None, point_dict=None,
                  channel_profiles=None, tdi_root_tool=None, name=""):
@@ -174,6 +174,17 @@ class SideViewPlotWidget(pg.PlotWidget):
         self.profile_route_updated.connect(self.update_water_level_cache)
         self.time_slider.valueChanged.connect(self.draw_waterlevel_line)
         self.time_slider.datasource_changed.connect(self.update_water_level_cache)
+
+        # set code for hovering
+        self.vb = self.plotItem.vb
+        self.proxy = pg.SignalProxy(self.scene().sigMouseMoved,
+                                    rateLimit=10,
+                                    slot=self.mouse_hover)
+        # self.scene().sigMouseMoved.connect(self.mouse_hover)
+
+    def mouse_hover(self, evt):
+        mouse_point = self.plotItem.vb.mapSceneToView(evt[0]).x()
+        self.profile_hovered.emit(mouse_point)
 
     def set_sideprofile(self, profile, route_points):
 
@@ -740,7 +751,12 @@ class CustomDistancePropeter(QgsArcProperter):
     def property(self, distance, feature):
         value = feature['real_length']
         if python_value(value) is None:
-            value = distance # feature.geometry().length()
+            # provided distance is not correct, so do a correct calculation
+            # value = distance
+            d = QgsDistanceArea()
+            value, unit = d.convertMeasurement(
+                feature.geometry().length(),
+                QGis.Degrees, QGis.Meters, False)
         return value
 
     def requiredAttributes(self):
@@ -748,13 +764,114 @@ class CustomDistancePropeter(QgsArcProperter):
         attributes = []
         return attributes
 
+class SideViewMapVisualisation(object):
+    # self.line_layer.crs()
+    def __init__(self, iface, source_crs):
+        self.iface = iface
+
+        self.source_crs = source_crs
+
+        # temp layer for side profile trac
+        self.rb = QgsRubberBand(self.iface.mapCanvas())
+        self.rb.setColor(Qt.red)
+        self.rb.setWidth(2)
+
+        # temp layer for last selected point
+        self.point_markers = []
+        self.active_route = None
+
+        self.hover_marker = QgsVertexMarker(self.iface.mapCanvas())
+        self.hover_marker.setIconType(QgsVertexMarker.ICON_X)
+        self.hover_marker.setColor(Qt.red)
+        self.hover_marker.setPenWidth(6)
+
+        self.dist_calc = QgsDistanceArea()
+
+    def close(self):
+        self.reset()
+        self.iface.mapCanvas().scene().removeItem(self.hover_marker)
+
+
+    def set_sideview_route(self, route):
+
+        self.reset()
+
+        self.active_route = route
+        transform = QgsCoordinateTransform(self.source_crs,
+                                           self.iface.mapCanvas().mapRenderer().destinationCrs())
+
+        for pnt in route.path_vertexes:
+            t_pnt = transform.transform(pnt)
+            self.rb.addPoint(t_pnt)
+
+
+        for point, point_id, dist in route.path_points:
+
+            marker = QgsVertexMarker(self.iface.mapCanvas())
+            marker.setIconType(QgsVertexMarker.ICON_CIRCLE)
+            marker.setColor(Qt.red)
+            marker.setPenWidth(4)
+            marker.setCenter(transform.transform(point))
+            self.point_markers.append(marker)
+
+
+    def reset(self):
+        self.rb.reset()
+        self.active_route = None
+
+        for marker in self.point_markers:
+            self.iface.mapCanvas().scene().removeItem(marker)
+
+        self.point_markers = []
+
+        self.hover_marker.setCenter(QgsPoint(0.0, 0.0))
+
+    def hover_graph(self, meters_from_start):
+
+        transform = QgsCoordinateTransform(self.source_crs,
+                                           self.iface.mapCanvas().mapRenderer().destinationCrs())
+
+        if self.active_route is None:
+            return
+
+        if meters_from_start < 0.0:
+            meters_from_start = 0.0
+        elif (len(self.active_route.path) > 0 and
+                        meters_from_start > self.active_route.path[-1][-1][1]):
+            meters_from_start = self.active_route.path[-1][-1][1]
+
+        for route_part in self.active_route.path:
+            if meters_from_start <= route_part[-1][1]:
+                for part in route_part:
+                    if meters_from_start <= part[1]:
+                        if part[3] == 1:
+                            distance_on_line = meters_from_start - part[0]
+                        else:
+                            distance_on_line = part[1] - meters_from_start
+
+                        length, unit_type = self.dist_calc.convertMeasurement(
+                            distance_on_line,
+                            QGis.Meters, QGis.Degrees, False)  # QGis.Degrees
+
+                        point = part[4].geometry().interpolate(length)
+                        self.hover_marker.setCenter(transform.transform(point.asPoint()))
+                        return
+
+    def hover_map(self, point_geometry):
+        pass
+
+    def show_selectable_points(self, graph_tree):
+        pass
+
+    def hide_selectable_points(self):
+        pass
+
+
 
 class SideViewDockWidget(QDockWidget):
     """Main Dock Widget for showing 3di results in Graphs"""
 
     # todo:
-    # toon vlaggetje bij geselecteerde punten
-    # meer punten achter elkaar selecteerbaar
     # punten verplaatsen
     # als op lijn wordt gedrukt en vastgehouden
     # detecteer dichtsbijzijnde punt in plaats van willekeurige binnen gebied
@@ -826,13 +943,11 @@ class SideViewDockWidget(QDockWidget):
 
         self.route_tool.deactivated.connect(self.unset_route_tool)
 
-        # temp layer for side profile trac
-        self.rb = QgsRubberBand(self.iface.mapCanvas())
-        self.rb.setColor(Qt.red)
-        self.rb.setWidth(2)
+        self.map_visualisation = SideViewMapVisualisation(self.iface,
+                                                          self.line_layer.crs())
 
-        # temp layer for last selected point
-        self.point_markers = QgsVertexMarker(self.iface.mapCanvas())
+        # connect graph hover to point visualisation on map
+        self.active_sideview.profile_hovered.connect(self.map_visualisation.hover_graph)
 
         # add tree layer to map (for fun and testing purposes)
         self.vl_tree_layer = self.route.get_virtual_tree_layer()
@@ -1135,7 +1250,7 @@ class SideViewDockWidget(QDockWidget):
                 cpoints_idx = []
                 cpoints = {}
                 # get calculation points on line
-                for line in channel_calc_points[str(channel['id'])]:
+                for line in channel_calc_points[channel['id']]:
                     cpoints_idx.append(line['start_node_idx'])
                     cpoints[line['start_node_idx']] = \
                                 line.geometry().asPolyline()[0]
@@ -1314,17 +1429,13 @@ class SideViewDockWidget(QDockWidget):
         self.active_sideview.set_sideprofile(self.route.path,
                                              self.route.path_points)
 
-        transform = QgsCoordinateTransform(self.line_layer.crs(),
-                                           self.iface.mapCanvas().mapRenderer().destinationCrs())
+        self.map_visualisation.set_sideview_route(self.route)
 
-        self.rb.reset()
-        for pnt in self.route.path_vertexes:
-            t_pnt = transform.transform(pnt)
-            self.rb.addPoint(t_pnt)
 
     def reset_sideview(self):
         self.route.reset()
-        self.rb.reset()
+        self.map_visualisation.reset()
+
         self.active_sideview.set_sideprofile([], [])
 
     def on_close(self):
@@ -1339,7 +1450,8 @@ class SideViewDockWidget(QDockWidget):
 
         self.unset_route_tool()
 
-        self.rb.reset()
+        self.active_sideview.profile_hovered.disconnect(self.map_visualisation.hover_graph)
+        self.map_visualisation.close()
 
         for sideview_plot in self.sideviews:
             sideview_plot[1].on_close()
