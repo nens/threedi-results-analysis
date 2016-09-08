a3c25e77250b2f4268ea572dcdc9c72e6f9da730
Jackie <Jackie.Leng@nelen-schuurmans.nl>
2016-09-08 11:44:34 +0200

    Checking

--- test/test_henk.py
+++ test/test_henk.py
@@ -33,30 +33,6 @@ netcdf_datasource_path = os.path.join(
     'data', 'testmodel', 'results', 'subgrid_map.nc')
 
 
-@unittest.skipIf(not os.path.exists(netcdf_datasource_path),
-                 "Path to test netcdf doesn't exist.")
-class TestNetcdfDatasource(unittest.TestCase):
-
-    def setUp(self):
-        self.ncds = NetcdfDataSource(netcdf_datasource_path,
-                                     load_properties=False)
-
-    def test_netcdf_loaded(self):
-        """We can open the Netcdf file"""
-        self.assertTrue(self.ncds.ds is not None)
-
-    def test_id_mapping_loaded(self):
-        """The datasource correctly finds the id_mapping.json."""
-        self.assertTrue(self.ncds.id_mapping is not None)
-
-    def test_timestamps(self):
-        """We'll asume there are always some time steps"""
-        ts = self.ncds.get_timestamps(object_type="doesn't matter")
-        self.assertTrue(len(ts) > 0)
-        self.assertEqual(ts[0], 0.0)
-        self.assertNotEqual(ts[1], 0.0)
-
-
 class TestNetcdfDatasourceBasic(unittest.TestCase):
     """Some basic tests without needing an actual netCDF file."""
 
