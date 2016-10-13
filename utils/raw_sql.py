cf2c4e2147522fec6dac9f5b07c65c041ae0c5bc
larsclaussen <lars.claussen@nelen-schuurmans.nl>
2016-10-13 21:34:11 +0200

    added sql query support for spatialite

--- utils/raw_sql.py
+++ utils/raw_sql.py
@@ -1,86 +1,119 @@
 # -*- coding: utf-8 -*-
-boundary_query_str = """
-    -- boundaries
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def get_model_data_queries(flavor):
+    """
+    get sql query strings for all 1D objects that are needed
+    to predict the 1D threedicore calculation points. Those are:
+        - boundary points
+        - manholes
+        - channels
+        - culverts
+        - pipes
+
+    :param flavor: database flavor, either 'sqlite' or 'postgres'
+    :return:
+    """
+    flavor = flavor.lower()
+    logger.info("[*] Getting sql queries for {}".format(flavor))
+
+    sql_functions_map = {
+        'makeline':
+          {'postgres': 'ST_MakeLine',
+           'sqlite': 'MakeLine'
+        },
+    }
+    queries = []
+    boundary_query_str = """
+        -- boundaries
+        SELECT
+          connection_node_id
+          , -1 AS calculation_type
+          , id
+        FROM
+          v2_1d_boundary_conditions
+        ;
+    """
+    queries.append(boundary_query_str)
+    manhole_query_str = """
+    -- manholes
     SELECT
       connection_node_id
-      , -1 AS calculation_type
+      , calculation_type
       , id
     FROM
-      v2_1d_boundary_conditions
+      v2_manhole
     ;
-"""
-manhole_query_str = """
--- manholes
-SELECT
-  connection_node_id
-  , calculation_type
-  , id
-FROM
-  v2_manhole
-;
-"""
-pipe_query_str = """
--- pipes
-SELECT
-  connection_node_start_id
-  , connection_node_end_id
-  , calculation_type
-  , ST_AsText(cn_start.the_geom) AS the_geom_start
-  , ST_AsText(cn_end.the_geom) AS the_geom_end
-  , ST_AsText(ST_MakeLine(cn_start.the_geom, cn_end.the_geom)) AS line
-  , ST_Length(ST_MakeLine(cn_start.the_geom, cn_end.the_geom)) as length
-  , p.id
-  , dist_calc_points
-FROM
-  v2_pipe AS p
-  ,v2_connection_nodes AS cn_start
-  ,v2_connection_nodes AS cn_end
-WHERE
-  connection_node_start_id = cn_start.id
-AND
-  connection_node_end_id = cn_end.id
-;
-"""
-culvert_query_str = """
--- culverts
-SELECT
-  connection_node_start_id
-  , connection_node_end_id
-  , 101
-  , ST_AsText(c.the_geom) AS the_geom
-  , ST_Length(c.the_geom) as length
-  , c.id
-  , dist_calc_points
-  , ST_AsText(cn_end.the_geom) AS the_geom_end
-FROM
-  v2_culvert AS c
-  ,v2_connection_nodes AS cn_start
-  ,v2_connection_nodes AS cn_end
-WHERE
-  connection_node_start_id = cn_start.id
-AND
-  connection_node_end_id = cn_end.id
-;
-"""
-channel_query_str = """
--- channels
-SELECT
-  connection_node_start_id
-  , connection_node_end_id
-  , calculation_type
-  , ST_AsText(cn_start.the_geom) AS the_geom_start
-  , ST_AsText(cn_end.the_geom) AS the_geom_end
-  , ST_AsText(c.the_geom) AS line
-  , ST_Length(c.the_geom) as length
-  , c.id
-  , dist_calc_points
-FROM
-  v2_channel AS c
-  ,v2_connection_nodes AS cn_start
-  ,v2_connection_nodes AS cn_end
-WHERE
-  connection_node_start_id = cn_start.id
-AND
-  connection_node_end_id = cn_end.id
-;
-"""
+    """
+    pipe_query_str = """
+    -- pipes
+    SELECT
+      connection_node_start_id
+      , connection_node_end_id
+      , calculation_type
+      , ST_AsText(cn_start.the_geom) AS the_geom_start
+      , ST_AsText(cn_end.the_geom) AS the_geom_end
+      , ST_AsText({makeline}(cn_start.the_geom, cn_end.the_geom)) AS line
+      , ST_Length({makeline}(cn_start.the_geom, cn_end.the_geom)) as length
+      , p.id
+      , dist_calc_points
+    FROM
+      v2_pipe AS p
+      ,v2_connection_nodes AS cn_start
+      ,v2_connection_nodes AS cn_end
+    WHERE
+      connection_node_start_id = cn_start.id
+    AND
+      connection_node_end_id = cn_end.id
+    ;
+    """.format(makeline=sql_functions_map['makeline'][flavor])
+    queries.append(pipe_query_str)
+    culvert_query_str = """
+    -- culverts
+    SELECT
+      connection_node_start_id
+      , connection_node_end_id
+      , 101
+      , ST_AsText(c.the_geom) AS the_geom
+      , ST_Length(c.the_geom) as length
+      , c.id
+      , dist_calc_points
+      , ST_AsText(cn_end.the_geom) AS the_geom_end
+    FROM
+      v2_culvert AS c
+      ,v2_connection_nodes AS cn_start
+      ,v2_connection_nodes AS cn_end
+    WHERE
+      connection_node_start_id = cn_start.id
+    AND
+      connection_node_end_id = cn_end.id
+    ;
+    """
+    queries.append(culvert_query_str)
+    channel_query_str = """
+    -- channels
+    SELECT
+      connection_node_start_id
+      , connection_node_end_id
+      , calculation_type
+      , ST_AsText(cn_start.the_geom) AS the_geom_start
+      , ST_AsText(cn_end.the_geom) AS the_geom_end
+      , ST_AsText(c.the_geom) AS line
+      , ST_Length(c.the_geom) as length
+      , c.id
+      , dist_calc_points
+    FROM
+      v2_channel AS c
+      ,v2_connection_nodes AS cn_start
+      ,v2_connection_nodes AS cn_end
+    WHERE
+      connection_node_start_id = cn_start.id
+    AND
+      connection_node_end_id = cn_end.id
+    ;
+    """
+    queries.append(channel_query_str)
+    return queries
