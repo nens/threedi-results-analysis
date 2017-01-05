04d3205746df256a217f03af6452d25f53264ce3
arnoldvantveld <arnold.vantveld@nelen-schuurmans.nl>
2017-01-05 11:01:30 +0100

    Update sideview.py with temporarily fix for pumps
    changed start_level_suction_side to start_level.
    and end_upper_level = start_upper_level
    code need to be cleaned up later by real programmer

--- views/sideview.py
+++ views/sideview.py
@@ -1171,10 +1171,10 @@ class SideViewDockWidget(QDockWidget):
 
         for pump in pump_layer.getFeatures():
 
-            start_upper_level = pump['start_level_suction_side']
-            end_upper_level = pump['start_level_delivery_side']
-            start_lower_level = pump['stop_level_suction_side']
-            end_lower_level = pump['stop_level_delivery_side']
+            start_upper_level = pump['start_level']
+            end_upper_level = start_upper_level
+            start_lower_level = pump['lower_stop_level']
+            end_lower_level = start_lower_level
             start_height = None
             end_height = None
 
