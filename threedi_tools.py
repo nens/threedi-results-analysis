a72a4983d66fc4798872044d72e67a3ff477e44c
Jackie Leng <Jackie.Leng@nelen-schuurmans.nl>
2017-06-01 11:22:31 +0200

    Put tools into module

--- threedi_tools.py
+++ threedi_tools.py
@@ -32,93 +32,17 @@ from PyQt4.QtGui import QAction, QIcon, QLCDNumber
 import resources  # NoQa
 
 # Import the code of the tools
-from .stats.utils import get_csv_layer_cache_files
+from .misc_tools import About, CacheClearer
 from .threedi_result_selection import ThreeDiResultSelection
 from .threedi_toolbox import ThreeDiToolbox
 from .threedi_graph import ThreeDiGraph
 from .threedi_sideview import ThreeDiSideView
 from .views.timeslider import TimesliderWidget
 from .views.map_animator import MapAnimator
-from .utils.user_messages import pop_up_info, pop_up_question, log
+from .utils.user_messages import log
 from .models.datasources import TimeseriesDatasourceModel
 from .utils.qprojects import ProjectStateMixin
 from .utils.layer_tree_manager import LayerTreeManager
-
-
-class About(object):
-    """Add 3Di logo and about info."""
-
-    def __init__(self, iface):
-        self.iface = iface
-        self.icon_path = ':/plugins/ThreeDiToolbox/icon.png'
-        self.menu_text = "3Di about"
-
-    def run(self):
-        """Shows dialog with version information."""
-        # todo: add version number and link to sites
-        with open(os.path.join(os.path.dirname(__file__),
-                  'version.rst'), 'r') as f:
-            version = f.readline().rstrip()
-
-        pop_up_info("3Di Tools versie %s" % version,
-                    "About", self.iface.mainWindow())
-
-    def on_unload(self):
-        pass
-
-
-class CacheClearer(object):
-    """Tool to delete cache files."""
-
-    def __init__(self, iface, ts_datasource):
-        """Constructor.
-
-        Args:
-            iface: QGIS interface
-            ts_datasource: TimeseriesDatasourceModel instance
-        """
-        self.iface = iface
-        self.icon_path = ':/plugins/ThreeDiToolbox/icon_broom.png'
-        self.menu_text = "Clear cache"
-        self.ts_datasource = ts_datasource
-
-    def run(self):
-        """Find cached spatialite and csv layer files for all items in the
-        TimeseriesDatasourceModel object and delete them.
-        """
-        spatialite_filepaths = [
-            item.spatialite_cache_filepath() for
-            item in self.ts_datasource.rows if
-            os.path.exists(item.spatialite_cache_filepath())
-        ]
-        result_dirs = [
-            os.path.dirname(item.file_path.value) for
-            item in self.ts_datasource.rows
-        ]
-        csv_filepaths = get_csv_layer_cache_files(*result_dirs)
-        # Note: convert to set because duplicates are possible if the same
-        # datasource is loaded multiple times
-        cached = set(spatialite_filepaths + csv_filepaths)
-        if not cached:
-            pop_up_info("No cached files found.")
-            return
-
-        yes = pop_up_question(
-            "The following files will be deleted:\n" +
-            ',\n'.join(cached) +
-            "\n\nContinue?")
-
-        if yes:
-            for f in cached:
-                try:
-                    os.remove(f)
-                except OSError:
-                    pop_up_info("Failed to delete %s." % f)
-            pop_up_info("Cache cleared. You may need to restart QGIS and "
-                        "reload your data.")
-
-    def on_unload(self):
-        pass
 
 
 class ThreeDiTools(QObject, ProjectStateMixin):
