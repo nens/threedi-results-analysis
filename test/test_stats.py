b395c6f04164ea84d79277935fdabf17925136f7
Jackie Leng <Jackie.Leng@nelen-schuurmans.nl>
2017-06-13 11:39:35 +0200

    more tests

--- test/test_stats.py
+++ test/test_stats.py
@@ -1,8 +1,22 @@
+from contextlib import contextmanager
+import os
+import tempfile
+import shutil
 import unittest
 
 from ThreeDiToolbox.stats.ncstats import NcStats
 from ThreeDiToolbox.stats.utils import get_default_csv_path
+from ThreeDiToolbox.stats.utils import get_csv_layer_cache_files
 from ThreeDiToolbox.stats.utils import STATS_CACHE_LAYER_POSTFIX
+
+
+@contextmanager
+def TemporaryDirectory():
+    name = tempfile.mkdtemp()
+    try:
+        yield name
+    finally:
+        shutil.rmtree(name)
 
 
 class TestNcStats(unittest.TestCase):
@@ -33,3 +47,17 @@ class TestStatsUtils(unittest.TestCase):
     def test_csv_path(self):
         p = get_default_csv_path('my_layer', '/home/homie')
         self.assertEqual(p, '/home/homie/my_layer' + STATS_CACHE_LAYER_POSTFIX)
+
+    def test_get_cache_files_empty(self):
+        with TemporaryDirectory() as tempdir:
+            files = get_csv_layer_cache_files(tempdir)
+            self.assertEqual(files, [])
+
+    def test_get_cache_files(self):
+        with TemporaryDirectory() as tempdir:
+            filepath = os.path.join(tempdir, 'my_file_stats.csv')
+            with open(filepath, 'w') as f:
+                f.write('jep')
+
+            files = get_csv_layer_cache_files(tempdir)
+            self.assertEqual(len(files), 1)
