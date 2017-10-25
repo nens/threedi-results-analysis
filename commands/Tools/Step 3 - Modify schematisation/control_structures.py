5542c5017ee43840e8379d223874590c1a32fbd9
GeOdin <madeleine.vanwinkel@gmail.com>
2017-10-25 10:34:37 +0200

    It is no longer possible to change measuring stations in the Measuring station tab. You can add and create new measuring stations in the measuring group dialog. The measuring station tab becomes an overview.

--- commands/Tools/Step 3 - Modify schematisation/control_structures.py
+++ commands/Tools/Step 3 - Modify schematisation/control_structures.py
@@ -85,29 +85,10 @@ class CustomCommand(CustomCommandBase):
         control_structure = ControlledStructures(
             flavor=db["db_entry"]['db_type'])
         control_structure.start_sqalchemy_engine(db["db_settings"])
-        self.update_connection_node_ids(control_structure)
-        self.update_measuring_point_ids(control_structure)
         self.update_measuring_group_ids(control_structure)
+        self.update_measuring_point_ids(control_structure)
         self.update_rule_ids(control_structure)
         self.update_control_ids(control_structure)
-
-    def update_connection_node_ids(self, control_structure):
-        self.dockwidget_controlled_structures.\
-            combobox_input_measuring_point_id.clear()
-        list_of_measuring_point_ids = control_structure.get_attributes(
-            table_name="v2_connection_nodes", attribute_name="id")
-        self.dockwidget_controlled_structures.\
-            combobox_input_measuring_point_id.addItems(
-                list_of_measuring_point_ids)
-
-    def update_measuring_point_ids(self, control_structure):
-        self.dockwidget_controlled_structures.\
-            combobox_input_measuring_point_view.clear()
-        list_of_measuring_group_ids = control_structure.get_attributes(
-            table_name="v2_control_measure_map", attribute_name="id")
-        self.dockwidget_controlled_structures.\
-            combobox_input_measuring_point_view.addItems(
-                list_of_measuring_group_ids)
 
     def update_measuring_group_ids(self, control_structure):
         self.dockwidget_controlled_structures.\
@@ -121,6 +102,15 @@ class CustomCommand(CustomCommandBase):
                 list_of_measuring_group_ids)
         self.dockwidget_controlled_structures.\
             combobox_input_measuring_group_delete.addItems(
+                list_of_measuring_group_ids)
+
+    def update_measuring_point_ids(self, control_structure):
+        self.dockwidget_controlled_structures.\
+            combobox_input_measuring_point_view.clear()
+        list_of_measuring_group_ids = control_structure.get_attributes(
+            table_name="v2_control_measure_map", attribute_name="id")
+        self.dockwidget_controlled_structures.\
+            combobox_input_measuring_point_view.addItems(
                 list_of_measuring_group_ids)
 
     def update_rule_ids(self, control_structure):
@@ -172,23 +162,8 @@ class CustomCommand(CustomCommandBase):
         self.dockwidget_controlled_structures\
             .pushbutton_input_measuring_point_clear.clicked\
             .connect(self.clear_measuring_point_table)
-        self.dockwidget_controlled_structures\
-            .pushbutton_input_measuring_point_new.clicked\
-            .connect(self.create_new_measuring_point)
         tablewidget = self.dockwidget_controlled_structures\
             .tablewidget_measuring_point
-        start_row = 0
-        tablewidget.setItem(start_row, 0, QTableWidgetItem(""))
-        tablewidget.setCellWidget(
-            start_row, 1, self.dockwidget_controlled_structures
-            .combobox_input_measuring_point_table)
-        tablewidget.setCellWidget(
-            start_row, 2, self.dockwidget_controlled_structures
-            .combobox_input_measuring_point_id)
-        tablewidget.setItem(start_row, 3, QTableWidgetItem(""))
-        tablewidget.setCellWidget(
-            start_row, 3, self.dockwidget_controlled_structures
-            .pushbutton_input_measuring_point_new)
         tablewidget.setEditTriggers(QAbstractItemView.NoEditTriggers)
 
     def setup_measuring_group_tab(self):
@@ -249,69 +224,6 @@ class CustomCommand(CustomCommandBase):
         self.remove_all_rule_tabs()
         self.remove_all_control_tabs()
 
-    def create_new_measuring_point(self):
-        """Create a new measuring point."""
-        # Get the model
-        db_key = self.dockwidget_controlled_structures\
-            .combobox_input_model.currentText()
-        db = get_database_properties(db_key)
-        control_structure = ControlledStructures(
-            flavor=db["db_entry"]['db_type'])
-        control_structure.start_sqalchemy_engine(db["db_settings"])
-        # Get last id of measure map or set to 0; set to +1
-        table_name = "v2_control_measure_map"
-        attribute_name = "MAX(id)"
-        try:
-            max_id_measure_map = int(control_structure.get_attributes(
-                table_name, attribute_name)[0])
-        except ValueError:
-            max_id_measure_map = 0
-        new_max_id_measure_map = max_id_measure_map + 1
-        # Populate the new row in the table
-        self.populate_measuring_point_row(new_max_id_measure_map)
-        # Insert the variables in the v2_control_table
-        measuring_point_table = self.dockwidget_controlled_structures\
-            .combobox_input_measuring_point_table.currentText()
-        measuring_point_table_id = self.dockwidget_controlled_structures\
-            .combobox_input_measuring_point_id.currentText()
-        attributes = {
-            "id": new_max_id_measure_map,
-            "object_type": measuring_point_table,
-            "object_id": measuring_point_table_id
-        }
-        control_structure.insert_into_table(table_name, attributes)
-        # Set the new ids of the v2_control_measure_map
-        self.update_measuring_point_ids(control_structure)
-
-    def populate_measuring_point_row(self, id_measuring_point):
-        """
-        Populate a row from te measuring point table.
-
-        Args:
-            (str) id_measuring_point: The id of the measuring point."""
-        tablewidget = self.dockwidget_controlled_structures\
-            .tablewidget_measuring_point
-        # Always put the new row on top.
-        row_position = 1
-        tablewidget.insertRow(row_position)
-        measuring_point_id = QTableWidgetItem(str(id_measuring_point))
-        tablewidget.setItem(row_position, 0, measuring_point_id)
-        measuring_point_table_widget = QTableWidgetItem(
-            self.dockwidget_controlled_structures
-            .combobox_input_measuring_point_table.currentText())
-        tablewidget.setItem(row_position, 1, measuring_point_table_widget)
-        measuring_point_table_id_widget = QTableWidgetItem(
-            self.dockwidget_controlled_structures
-            .combobox_input_measuring_point_id.currentText())
-        tablewidget.setItem(row_position, 2, measuring_point_table_id_widget)
-        measuring_point_remove_widget = QPushButton("Remove")
-        tablewidget = self.dockwidget_controlled_structures\
-            .tablewidget_measuring_point
-        measuring_point_remove_widget.clicked.connect(
-            self.remove_measuring_point_row)
-        tablewidget.setCellWidget(
-            row_position, 3, measuring_point_remove_widget)
-
     def view_measuring_point(self):
         """View a measuring station in 'Ḿeasuring station' tab."""
         tablewidget = self.dockwidget_controlled_structures\
@@ -331,7 +243,7 @@ class CustomCommand(CustomCommandBase):
             table_name=table_name, attribute_name=attribute_name,
             where=where)[0]
         # Insert on top of the table
-        row_position = 1
+        row_position = 0
         tablewidget.insertRow(row_position)
         measuring_point_id = QTableWidgetItem(str(measure_point[0]))
         tablewidget.setItem(row_position, 0, measuring_point_id)
@@ -339,9 +251,6 @@ class CustomCommand(CustomCommandBase):
         tablewidget.setItem(row_position, 1, measuring_point_table)
         measuring_point_table_id = QTableWidgetItem(str(measure_point[3]))
         tablewidget.setItem(row_position, 2, measuring_point_table_id)
-        measuring_point_remove = QPushButton("Remove")
-        measuring_point_remove.clicked.connect(self.remove_measuring_point_row)
-        tablewidget.setCellWidget(row_position, 3, measuring_point_remove)
 
     def view_all_measuring_points(self):
         """View all the measuring points in the Measuring station tab."""
@@ -368,45 +277,14 @@ class CustomCommand(CustomCommandBase):
             tablewidget.setItem(row_position, 1, measuring_point_table)
             measuring_point_table_id = QTableWidgetItem(str(measure_point[3]))
             tablewidget.setItem(row_position, 2, measuring_point_table_id)
-            measuring_point_remove = QPushButton("Remove")
-            measuring_point_remove.clicked.connect(
-                self.remove_measuring_point_row)
-            tablewidget.setCellWidget(row_position, 3, measuring_point_remove)
-
-    def remove_measuring_point_row(self):
-        """Remove a row from the measuring point table."""
-        tablewidget = self.dockwidget_controlled_structures\
-            .tablewidget_measuring_point
-        row_number = tablewidget.currentRow()
-        # Get id of measuring point
-        measuring_point_id = tablewidget.item(row_number, 0).text()
-        # Remove measuring point from database
-        db_key = self.dockwidget_controlled_structures\
-            .combobox_input_model.currentText()  # name of database
-        db = get_database_properties(db_key)
-        control_structure = ControlledStructures(
-            flavor=db["db_entry"]['db_type'])
-        control_structure.start_sqalchemy_engine(db["db_settings"])
-        table_name = "v2_control_measure_map"
-        where = " WHERE id = '{}'".format(str(measuring_point_id))
-        control_structure.delete_from_database(
-            table_name=table_name, where=where)
-        # Update measuring point ids
-        self.update_measuring_point_ids(control_structure)
-        # Remove measuring point from dockwidget
-        # Don't remove the first row.
-        dont_remove = 0
-        if row_number != dont_remove:
-            tablewidget.removeRow(row_number)
 
     def clear_measuring_point_table(self):
         """Clear the measuring point table."""
-        # Leave the first row standing.
         row_count = self.dockwidget_controlled_structures\
             .tablewidget_measuring_point.rowCount()
-        for row in range(row_count - 1):
+        for row in range(row_count):
             self.dockwidget_controlled_structures\
-                .tablewidget_measuring_point.removeRow(1)
+                .tablewidget_measuring_point.removeRow(0)
 
     def create_new_measuring_group(self):
         """Create a new measuring group."""
@@ -432,6 +310,7 @@ class CustomCommand(CustomCommandBase):
                 dockwidget_controlled_structures=self.
                 dockwidget_controlled_structures)
         self.dialog_create_measuring_group.exec_()  # block execution
+        self.update_measuring_point_ids(control_structure)
         self.update_measuring_group_ids(control_structure)
 
     def view_measuring_group(self):
--- views/control_structures_create_measuring_group.py
+++ views/control_structures_create_measuring_group.py
@@ -69,7 +69,8 @@ class CreateMeasuringGroupDialogWidget(QDialog, FORM_CLASS):
         self.db = get_database_properties(self.db_key)
         self.control_structure = ControlledStructures(
             flavor=self.db["db_entry"]['db_type'])
-        self.setup_ids()
+        self.setup_tablewidget()
+        self.update_ids()
         self.connect_signals()
 
     def on_accept(self):
@@ -94,7 +95,13 @@ class CreateMeasuringGroupDialogWidget(QDialog, FORM_CLASS):
 
         event.accept()
 
-    def setup_ids(self):
+    def setup_tablewidget(self):
+        tablewidget = self.tablewidget_measuring_point
+        tablewidget.setCellWidget(0, 0, self.combobox_input_measuring_point_table)
+        tablewidget.setCellWidget(0, 1, self.combobox_input_measuring_point_id)
+        tablewidget.setCellWidget(0, 3, self.pushbutton_input_measuring_point_new)
+
+    def update_ids(self):
         """Setup the id's for the measuring group and measuring points."""
         # Set the id of the measuring group
         self.label_measuring_group_id_info.setText(self.measuring_group_id)
@@ -111,15 +118,76 @@ class CreateMeasuringGroupDialogWidget(QDialog, FORM_CLASS):
             table_name="v2_control_measure_map", attribute_name="id")
         self.combobox_measuring_point_load.addItems(
             list_of_measuring_point_ids)
+        # Set all id's of the connection nodes
+        self.combobox_input_measuring_point_id.clear()
+        list_of_connection_node_ids = self.control_structure.get_attributes(
+            table_name="v2_connection_nodes", attribute_name="id")
+        self.combobox_input_measuring_point_id.addItems(list_of_connection_node_ids)
 
     def connect_signals(self):
         """Connect the signals."""
         self.pushbutton_measuring_group_load.clicked.connect(
             self.load_measuring_group)
+        self.pushbutton_input_measuring_point_new.clicked.connect(
+            self.create_new_measuring_point)
         self.pushbutton_measuring_point_load.clicked.connect(
             self.load_measuring_point)
         self.buttonbox.accepted.connect(self.on_accept)
         self.buttonbox.rejected.connect(self.on_reject)
+
+    def create_new_measuring_point(self):
+        # Get the model
+        self.control_structure.start_sqalchemy_engine(self.db["db_settings"])
+        # Get last id of measure map or set to 0; set to +1
+        table_name = "v2_control_measure_map"
+        attribute_name = "MAX(id)"
+        try:
+            max_id_measure_map = int(self.control_structure.get_attributes(
+                table_name, attribute_name)[0])
+        except ValueError:
+            max_id_measure_map = 0
+        new_max_id_measure_map = max_id_measure_map + 1
+        # Populate the new row in the table
+        self.populate_measuring_point_row(new_max_id_measure_map)
+
+    def populate_measuring_point_row(self, id_measuring_point):
+        """
+        Populate a row from the measuring point table.
+
+        Args:
+            (str) id_measuring_point: The id of the measuring point."""
+        tablewidget = self.tablewidget_measuring_point
+        # Always put the new row on top.
+        row_position = 1
+        tablewidget.insertRow(row_position)
+        measuring_point_id = QTableWidgetItem(str(id_measuring_point))
+        # tablewidget.setItem(row_position, 0, measuring_point_id)
+        measuring_point_table_widget = QTableWidgetItem(
+            self.combobox_input_measuring_point_table.currentText())
+        tablewidget.setItem(row_position, 0, measuring_point_table_widget)
+        measuring_point_table_id_widget = QTableWidgetItem(
+            self.combobox_input_measuring_point_id.currentText())
+        tablewidget.setItem(row_position, 1, measuring_point_table_id_widget)
+        try:
+            measuring_point_weight = tablewidget.item(0, 2).text()
+        except AttributeError:
+            measuring_point_weight = ""
+        tablewidget.setItem(row_position, 2, QTableWidgetItem(measuring_point_weight))
+        measuring_point_remove_widget = QPushButton("Remove")
+        measuring_point_remove_widget.clicked.connect(
+            self.remove_measuring_point_row)
+        tablewidget.setCellWidget(
+            row_position, 3, measuring_point_remove_widget)
+
+    def remove_measuring_point_row(self):
+        """Remove a row from the measuring point table."""
+        tablewidget = self.tablewidget_measuring_point
+        row_number = tablewidget.currentRow()
+        # Remove measuring point from dockwidget
+        # Don't remove the first row.
+        BUTTON_ROW = 0
+        if row_number != BUTTON_ROW:
+            tablewidget.removeRow(row_number)
 
     def load_measuring_point(self):
         """Load a measuring group in the tablewidget."""
@@ -133,7 +201,7 @@ class CreateMeasuringGroupDialogWidget(QDialog, FORM_CLASS):
             table_name=table_name, attribute_name=attribute_name,
             where=where_clause)[0]
         # The measuring point should be added on the top of the table
-        row_position = 0
+        row_position = 1
         self.tablewidget_measuring_point.insertRow(row_position)
         self.tablewidget_measuring_point.setItem(
             row_position, 0, QTableWidgetItem(str(measure_point[2])))
@@ -148,10 +216,12 @@ class CreateMeasuringGroupDialogWidget(QDialog, FORM_CLASS):
 
     def load_measuring_group(self):
         """Load a measuring group in the tablewidget."""
-        # Remove all current rows.
-        row_count = self.tablewidget_measuring_point.rowCount()
-        for row in range(row_count):
-            self.tablewidget_measuring_point.removeRow(0)
+        # Remove all current rows, besides te first.
+        tablewidget = self.tablewidget_measuring_point
+        BUTTON_ROW = 0
+        row_count = tablewidget.rowCount()
+        for row in range(row_count - 1):
+            tablewidget.removeRow(1)
         self.control_structure.start_sqalchemy_engine(self.db["db_settings"])
         # Get all the measuring points from a certain measure group
         table_name = "v2_control_measure_map"
@@ -195,24 +265,26 @@ class CreateMeasuringGroupDialogWidget(QDialog, FORM_CLASS):
         # the controlled structures dockwidget
         self.add_measuring_group_tab_dockwidget()
         table_name = "v2_control_measure_map"
+        BUTTON_ROW = 0
         for row in range(self.tablewidget_measuring_point.rowCount()):
-            # Get the new measuring_point id
-            attribute_name = "MAX(id)"
-            try:
-                max_id_measure_point = int(
-                    self.control_structure.get_attributes(
-                        table_name, attribute_name)[0])
-            except ValueError:
-                max_id_measure_point = 0
-            new_measuring_point_id = max_id_measure_point + 1
-            measure_point_attributes = self.get_measuring_point_attributes(
-                row, new_measuring_point_id)
-            # Save the measuring point in the v2_control_measure_map
-            self.control_structure.insert_into_table(
-                table_name, measure_point_attributes)
-            # Setup new tab of "Measuring group" tab
-            self.setup_measuring_group_table_dockwidget(
-                measure_point_attributes)
+            if row != BUTTON_ROW:
+                # Get the new measuring_point id
+                attribute_name = "MAX(id)"
+                try:
+                    max_id_measure_point = int(
+                        self.control_structure.get_attributes(
+                            table_name, attribute_name)[0])
+                except ValueError:
+                    max_id_measure_point = 0
+                new_measuring_point_id = max_id_measure_point + 1
+                measure_point_attributes = self.get_measuring_point_attributes(
+                    row, new_measuring_point_id)
+                # Save the measuring point in the v2_control_measure_map
+                self.control_structure.insert_into_table(
+                    table_name, measure_point_attributes)
+                # Setup new tab of "Measuring group" tab
+                self.setup_measuring_group_table_dockwidget(
+                    measure_point_attributes)
 
     def add_measuring_group_tab_dockwidget(self):
         """
