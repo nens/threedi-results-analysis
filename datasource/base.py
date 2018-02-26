ef2e854b032b946ebbee474cf80f0b8ba16fccb7
Jackie Leng <Jackie.Leng@nelen-schuurmans.nl>
2018-02-26 10:43:44 +0100

    add datasource interface (#203)
    * Add BaseDataSource interface
    
    * comment
    
    * changes

---
+++ datasource/base.py
@@ -0,0 +1,60 @@
+from abc import (
+    ABCMeta,
+    abstractmethod,
+)
+
+
+class BaseDataSource(object):
+    # NOTE: methods used in ncstats methods are ignored for now to keep
+    # things more manageable....
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def __init__(self, *args, **kwargs):
+        pass
+
+    @abstractmethod
+    def available_subgrid_map_vars(self):
+        pass
+
+    @abstractmethod
+    def available_aggregation_vars(self):
+        pass
+
+    @abstractmethod
+    def node_type_of(self, node_idx):
+        pass
+
+    @abstractmethod
+    def line_type_of(self, line_idx):
+        pass
+
+    @abstractmethod
+    def get_timeseries(
+            self, object_type, object_id, nc_variable, fill_value=None):
+        pass
+
+    @abstractmethod
+    def get_timestamps(self, object_type=None, parameter=None):
+        pass
+
+    # used in map_animator
+    @abstractmethod
+    def get_values_by_timestep_nr(self, variable, timestamp_idx, index=None):
+        pass
+
+    # TODO: not sure if needed, used in creating layers
+    @abstractmethod
+    def channel_mapping(self):
+        pass
+
+    # TODO: not sure if needed, used in creating layer
+    @abstractmethod
+    def node_mapping(self):
+        pass
+
+    # TODO: used in creating layer in NetcdfDataSource, but should be
+    # refactored out because the new DataSource uses hdf5
+    @abstractmethod
+    def id_mapping(self):
+        pass
