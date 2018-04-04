b36417b18f5ce7d5e4c3745247bceeb9f294d145
Jackie Leng <Jackie.Leng@nelen-schuurmans.nl>
2018-04-04 15:31:31 +0200

    quick and dirty implementation of available_subgrid_map_vars (#213)
    * quick and dirty implementation of available_subgrid_map_vars
    
    * More clarification
    
    * put vars into constants
    
    * Add doctest
    
    * Fix doctest
    
    * Fix qlat incorrect name
    
    * Changes
    
    * Use namedtuple attribute name instead of indexed form

--- datasource/base.py
+++ datasource/base.py
@@ -64,15 +64,54 @@ class BaseDataSource(object):
 
 
 class DummyDataSource(BaseDataSource):
+    PREFIX_1D = 'Mesh1D_'
+    PREFIX_2D = 'Mesh2D_'
+    PREFIX_1D_LENGTH = 7  # just so we don't have to recalculate
+    PREFIX_2D_LENGTH = 7  # just so we don't have to recalculate
+
     def __init__(self, file_path=None, *args, **kwargs):
         from netCDF4 import Dataset
         self.file_path = file_path
         self._ga = None
         self.ds = Dataset(file_path)
 
+    def _strip_prefix(self, var_name):
+        """Strip away netCDF variable name prefixes.
+
+        Example variable names: 'Mesh2D_s1', 'Mesh1D_s1'
+
+        >>> from ThreeDiToolbox.datasource.base import DummyDataSource
+        >>> ds = DummyDataSource()
+        >>> ds._strip_prefix('Mesh2D_s1')
+        's1'
+        >>> ds._strip_prefix('Mesh1D_q')
+        'q'
+        >>> ds._strip_prefix('iets_anders')
+        'iets_anders'
+        """
+        if var_name.startswith(self.PREFIX_1D):
+            return var_name[self.PREFIX_1D_LENGTH:]
+        elif var_name.startswith(self.PREFIX_2D):
+            return var_name[self.PREFIX_2D_LENGTH:]
+        else:
+            return var_name
+
     @cached_property
     def available_subgrid_map_vars(self):
-        return []
+        """Available variables from 'subgrid_map.nc'."""
+        from .netcdf import SUBGRID_MAP_VARIABLES
+        known_subgrid_map_vars = set([v.name for v in SUBGRID_MAP_VARIABLES])
+        raw_available_vars = [
+            v for v in self.ds.variables.keys() if
+            v.startswith(self.PREFIX_1D) or v.startswith(self.PREFIX_2D)]
+        # Convert to a set to (1) get rid of duplicate variable names, and (2)
+        # to intersect with known variables
+        available_vars = set(
+            [self._strip_prefix(v) for v in raw_available_vars])
+        # filter using a hardcoded 'whitelist'
+        available_known_vars = available_vars.intersection(
+            known_subgrid_map_vars)
+        return list(available_known_vars)
 
     @cached_property
     def available_aggregation_vars(self):
