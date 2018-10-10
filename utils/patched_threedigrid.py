261c79820a7620022841524b0e315f34b324a49a
richard <richard.boon@nelen-schuurmans.nl>
2018-10-10 11:01:50 +0200

    Make the qgis plugin reloadable.

--- utils/patched_threedigrid.py
+++ utils/patched_threedigrid.py
@@ -12,7 +12,11 @@ the best way for now is to import ``GridH5Admin`` from this module, e.g.:
 
 
 from threedigrid.admin.lines.models import Lines
-del Lines.line_geometries
+try:
+    del Lines.line_geometries
+except AttributeError:
+    pass
+
 from threedigrid.admin.gridresultadmin import GridH5AggregateResultAdmin  # noqa
 from threedigrid.admin.gridadmin import GridH5Admin  # noqa
 from threedigrid.admin.gridresultadmin import GridH5ResultAdmin  # noqa
