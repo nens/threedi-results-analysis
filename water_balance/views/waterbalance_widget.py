5c41242b587f0c9b84cc3abeb155a4940ad6c79b
renierkramer <renier.kramer@nelen-schuurmans.nl>
2018-10-19 12:01:32 +0200

    start of debug 1d2d

--- water_balance/views/waterbalance_widget.py
+++ water_balance/views/waterbalance_widget.py
@@ -708,12 +708,16 @@ class WaterBalanceWidget(QDockWidget):
         bm_1d = BarManager(io_series_1d)
 
         bm_net.calc_balance(ts, ts_series, t1, t2, net=True)
+
+        # renier
         print 'break0'
+        
         bm_2d.calc_balance(ts, ts_series, t1, t2)
         bm_2d_groundwater.calc_balance(ts, ts_series, t1, t2, invert=[
             'infiltration/exfiltration (domain exchange)'])
         bm_1d.calc_balance(ts, ts_series, t1, t2, invert=['1D-2D exchange'])
 
+        # renier
         print 'break1'
 
         # init figure
