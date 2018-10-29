40abbc11da68a0f81299d424dd3b809c4fa2bf71
renierkramer <renier.kramer@nelen-schuurmans.nl>
2018-10-29 15:02:45 +0100

    [DOING] Renier disabled to select agg_netcdf as result_netcdf (#290)
    * disable select agg nc
    
    * disable select agg nc_2

--- views/result_selection.py
+++ views/result_selection.py
@@ -206,10 +206,11 @@ class ThreeDiResultSelectionWidget(QWidget, FORM_CLASS):
         except TypeError:
             init_path = os.path.expanduser("~")
 
-        filename = QFileDialog.getOpenFileName(self,
-                                               'Open resultaten file',
-                                               init_path,
-                                               'NetCDF (*.nc)')
+        filename = QFileDialog.getOpenFileNames(
+            self,
+            'Open results file',
+            init_path,
+            'NetCDF (subgrid_map.nc results_3di.nc)')
 
         if filename:
             # Little test for checking if there is an id mapping file available
