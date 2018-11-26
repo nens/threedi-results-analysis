ae6f148cf0e7cf3cb9cd2b456802db0bb3fd961b
richard <richard.boon@nelen-schuurmans.nl>
2018-11-26 10:48:58 +0100

    WIP: updated dockerfiles

--- Docker/qgis2/docker-entrypoint.sh
+++ Docker/qgis2/docker-entrypoint.sh
@@ -3,9 +3,8 @@
 USERNAME=$(cut -d: -f1 /etc/passwd | tail -1)
 USER_ID=$(id -u ${USERNAME})
 
-if [ "$1" = '/usr/bin/qgis' ]; then
-    chown $USERNAME:$USER_ID . -R
-    sh gosu $USERNAME:$USER_ID /usr/bin/qgis
+if [ "$1" = 'bash' ]; then
+    exec bash
 fi
 
-gosu $USERNAME:$USER_ID "${@}"
+exec gosu $USERNAME:$USER_ID "${@}"
