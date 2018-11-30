97f1751f42c140a90a04ca2be3f65a0cf13bbee0
richard <richard.boon@nelen-schuurmans.nl>
2018-11-30 16:50:30 +0100

    Added stats again.

---
+++ stats/ncstats.py
@@ -0,0 +1,273 @@
+"""
+This code was basically copied from python-flow with modifications to make
+it work with our own data sources.
+"""
+from builtins import object
+import numpy as np
+import inspect
+
+from ..datasource.netcdf import NetcdfDataSource, normalized_object_type
+from ..utils.user_messages import log
+
+
+def tailored_args(f, **kwargs):
+    """Filter and apply relevant kwargs to function."""
+    relevant_args = {k: v for (k, v) in list(kwargs.items()) if k
+                     in inspect.getargspec(f).args}
+    return f(**relevant_args)
+
+
+class NcStats(object):
+    """Get basic stats about subgrid netCDF files"""
+
+    # TODO: there are possible issues when you have arrays with shapes or
+    # lengths of 2. I.e. a q slice array has a shape of (2,), and when
+    # the last element is truncated you will get an array of shape (1,)
+    # which can be broadcasted together with everything. In practise there
+    # should probably be no issues since the time and q arrays are of the
+    # same length, but you never know. Might need some investigation.
+
+    # Update these lists if you add a new method
+    AVAILABLE_STRUCTURE_PARAMETERS = [
+        'q_cumulative_duration', 'q_end', 'tot_vol_positive',
+        'tot_vol_negative', 'time_q_max']
+    AVAILABLE_MANHOLE_PARAMETERS = ['s1_end', 'wos_duration']
+    AVAILABLE_PUMP_PARAMETERS = ['tot_vol_pump', 'pump_duration']
+
+    def __init__(self, netcdf_file_path=None, ds=None, datasource=None):
+        """
+        NOTE: ds arg is not supported at this time!!!
+
+        Args:
+            netcdf_file_path: path to netcdf
+            ds: netcdf dataset
+            datasource: NetcdfDataSource object
+        """
+        if datasource:
+            self.datasource = datasource
+        elif not ds and netcdf_file_path:
+            raise NotImplementedError('DEPRECATED')
+        elif ds:
+            # TODO: needs fixing
+            raise NotImplementedError('TODO')
+        else:
+            raise ValueError("No netCDF source")
+
+    @property
+    def timesteps(self):
+        return self.datasource.timesteps
+
+    @property
+    def timestamps(self):
+        return self.datasource.timestamps
+
+    def tot_vol_positive(self, structure_type, obj_id):
+        """Total volume through structure, counting only positive q's."""
+        q_slice = self.datasource.get_values_by_id(
+            'q', structure_type, object_id=obj_id)
+        # mask negative values
+        ma_q_slice = np.ma.masked_where(q_slice < 0, q_slice)
+        # calc total vol thru structure
+        vols = self.timesteps * ma_q_slice[0:-1]
+        return vols.sum()
+
+    def tot_vol_negative(self, structure_type, obj_id):
+        """Total volume through structure, counting only negative q's."""
+        q_slice = self.datasource.get_values_by_id(
+            'q', structure_type, object_id=obj_id)
+        # mask positive values
+        ma_q_slice = np.ma.masked_where(q_slice > 0, q_slice)
+        # calc total vol thru structure
+        vols = self.timesteps * ma_q_slice[0:-1]
+        return vols.sum()
+
+    def tot_vol_pump(self, structure_type, obj_id):
+        """Total volume through a pump calculated using integration.
+
+        Note: for pumps we know that q_pump is always positive.
+        """
+        q_slice = self.datasource.get_values_by_id(
+            'q_pump', structure_type, object_id=obj_id)
+        # calc total vol thru structure
+        vols = self.timesteps * q_slice[0:-1]
+        return vols.sum()
+
+    def pump_duration(self, structure_type, obj_id, capacity_L_per_sec=None):
+        vol_pump = self.tot_vol_pump(structure_type, obj_id)
+        duration_secs = 1000 * vol_pump / capacity_L_per_sec
+        return duration_secs
+
+    def q_max(self, structure_type, obj_id):
+        """Maximum value of a q timeseries; can be negative.
+        """
+        q_slice = self.datasource.get_values_by_id(
+            'q', structure_type, object_id=obj_id)
+        _min = q_slice.min()
+        _max = q_slice.max()
+        # return highest absolute value, while retaining the sign of the number
+        return max(_min, _max, key=abs)
+
+    def time_q_max(self, structure_type, obj_id):
+        """The time at maximum value of a q timeseries
+        """
+        q_slice = self.datasource.get_values_by_id(
+            'q', structure_type, object_id=obj_id)
+        _min = q_slice.min()
+        _max = q_slice.max()
+        # return highest absolute value, while retaining the sign of the number
+        largest = max(_min, _max, key=abs)
+        (rows,) = np.where(q_slice == largest)
+        return self.timestamps[rows[0]]
+
+    def s1_max(self, structure_type, obj_id):
+        """Maximum value of a s1 timeseries."""
+        s1_slice = self.datasource.get_values_by_id(
+            's1', structure_type, object_id=obj_id)
+        return s1_slice.max()
+
+    def s1_end(self, structure_type, obj_id):
+        """Last value of a s1 timeseries."""
+        s1_slice = self.datasource.get_values_by_id(
+            's1', structure_type, object_id=obj_id)
+        return s1_slice[-1]
+
+    def q_cumulative_duration(self, structure_type, obj_id, threshold=None):
+        """Cumulative duration of all nonzero occurences of q.
+        """
+        if threshold:
+            # TODO: q values can be vary small, maybe add a threshold??
+            raise NotImplementedError()
+        q_slice = self.datasource.get_values_by_id(
+            'q', structure_type, object_id=obj_id)
+
+        # normalize nonzero qs to 1, so it becomes a binary representation
+        # of q, which we can simply multiply with the timesteps
+        q_slice = np.absolute(q_slice)
+        q_slice[q_slice > 0] = 1.
+        cum_duration = self.timesteps * q_slice[0:-1]
+        return cum_duration.sum()
+
+    def wos_duration(self, structure_type, obj_id, surface_level=None):
+        """Cumulative duration of s1 when there is 'water op straat'.
+
+        This means, count the timesteps where s1 - surface level > 0.
+        """
+        s1_slice = self.datasource.get_values_by_id(
+            's1', structure_type, object_id=obj_id)
+
+        water_op_straat = s1_slice - surface_level
+
+        # Normalize >0 to 1, and <0 to 0, so that we have a binary
+        # representation of values we can multiply with the time steps.
+        water_op_straat[water_op_straat > 0] = 1.
+        water_op_straat[water_op_straat < 0] = 0
+        cum_duration = self.timesteps * water_op_straat[0:-1]
+        return cum_duration.sum()
+
+    def q_end(self, structure_type, obj_id):
+        """q at last timeSTAMP (!= timestep)
+        """
+        q_slice = self.datasource.get_values_by_id(
+            'q', structure_type, object_id=obj_id)
+        return q_slice[-1]
+
+    def _get_value_from_parameter(
+            self, structure_type, obj_id, parameter_name, **kwargs):
+        """Select the method from parameter name and call the method.
+
+        Additional kwargs can be passed on.
+        """
+        method = getattr(self, parameter_name)
+        return tailored_args(method,
+                             structure_type=structure_type,
+                             obj_id=obj_id,
+                             **kwargs)
+
+    # Make NcStats have the same interface as NcStatsAgg
+    get_value_from_parameter = _get_value_from_parameter
+
+    def close(self):
+        # TODO: is this used? also law of demeter
+        self.datasource.ds.close()
+
+
+class NcStatsAgg(NcStats):
+    """A version of NcStats that works with the so called 'aggregation'
+    version of the 3Di netCDF files."""
+
+    # Update these lists if you add a new method
+    AVAILABLE_STRUCTURE_PARAMETERS = ['q_cum', 'q_max', 'q_min'] + \
+        NcStats.AVAILABLE_STRUCTURE_PARAMETERS
+    AVAILABLE_MANHOLE_PARAMETERS = NcStats.AVAILABLE_MANHOLE_PARAMETERS + \
+        ['wos_height', 's1_max', 'water_depth']
+    AVAILABLE_PUMP_PARAMETERS = ['q_pump_cum'] \
+        + NcStats.AVAILABLE_PUMP_PARAMETERS
+
+    def __init__(self, *args, **kwargs):
+        super(NcStatsAgg, self).__init__(*args, **kwargs)
+
+        if (not isinstance(self.datasource, NetcdfDataSource) or
+                self.datasource.ds_aggregation is None):
+            raise ValueError("No aggration netcdf available in data source.")
+
+        # Construct a dict with thing we actually have in the loaded netcdf,
+        # and store netcdf arrays in memory.
+        self.variables = dict()
+        for p in (self.AVAILABLE_MANHOLE_PARAMETERS +
+                  self.AVAILABLE_STRUCTURE_PARAMETERS +
+                  self.AVAILABLE_PUMP_PARAMETERS):
+            try:
+                copied_array = self.datasource.ds_aggregation.variables[p][:]
+                self.variables[p] = copied_array
+            except KeyError:
+                continue
+
+        # Generate result statistics a priori
+        for k, v in list(self.variables.items()):
+            if k.endswith('_cum'):
+                # We can sum without integration because parameter is already
+                # an integrated variable.
+                calcd_array = v.sum(0)
+            elif k.endswith('_min'):
+                calcd_array = v.min(0)
+            elif k.endswith('_max'):
+                calcd_array = v.max(0)
+            else:
+                raise ValueError("Unknown variable")
+            self.variables[k] = calcd_array
+        self.variable_keys = list(self.variables.keys())
+
+    def get_value_from_parameter(self, structure_type, obj_id,
+                                 parameter_name, **kwargs):
+        """Select array and get the value.
+
+        Args:
+            structure_type: raw layer name; structure type normalization,
+                is delegated to the NetcdfDataSource
+            obj_id: layer feature id?? -------------> TODO: needs checking!!!
+            parameter_name: the netcdf variable name
+            kwargs: unused, but needed to be compatible with NcStats
+        """
+        norm_object_type = normalized_object_type(structure_type)
+        netcdf_id = self.datasource.obj_to_netcdf_id(obj_id, norm_object_type)
+        if parameter_name in self.variable_keys:
+            variable = self.variables[parameter_name]
+            return variable[netcdf_id]
+        else:
+            log("This aggregation variable has no stats (%s), attempting "
+                "lookup in regular NcStats." % parameter_name)
+            # The variable was not found in aggregation netCDF. We will look
+            # further down in the regular netCDF.
+            variable = self._get_value_from_parameter(
+                structure_type, obj_id, parameter_name, **kwargs)
+            return variable
+
+    def wos_height(self, structure_type, obj_id, surface_level=None):
+        s1_max = self.get_value_from_parameter(
+            structure_type, obj_id, 's1_max')
+        return s1_max - surface_level
+
+    def water_depth(self, structure_type, obj_id, bottom_level=None):
+        s1_max = self.get_value_from_parameter(
+            structure_type, obj_id, 's1_max')
+        return s1_max - bottom_level
---
+++ stats/utils.py
@@ -0,0 +1,406 @@
+import os
+import csv
+import logging
+
+from qgis.core import (
+    QgsVectorLayer,
+    QgsProject,
+    QgsVectorLayerJoinInfo,
+)
+
+from .ncstats import NcStats, NcStatsAgg
+
+log = logging.getLogger(__name__)
+
+STATS_LAYER_IDENTIFIER = 'stats'  # stats layers contain this string
+STATS_CACHE_LAYER_POSTFIX = '_{}.csv'.format(STATS_LAYER_IDENTIFIER)
+
+
+def get_default_csv_path(layer_name, result_dir):
+    """The default file path the stats generating functions in this module
+    write to."""
+    filename = layer_name + STATS_CACHE_LAYER_POSTFIX
+    return os.path.join(result_dir, filename)
+
+
+def get_csv_layer_cache_files(*directories):
+    """Get paths to the csv cache files."""
+    return [
+        os.path.join(_dir, f) for _dir in directories
+        for f in os.listdir(_dir)
+        if f.endswith(STATS_CACHE_LAYER_POSTFIX)]
+
+
+def csv_join(filepath, layer, view_layer_field, csv_field='id',
+             add_to_legend=True):
+    """Generate a layer from csv file and join it with another vector layer.
+
+    Args:
+        filepath: path to the csv file
+        layer: the layer we want to join the csv with
+        view_layer_field: the id (e.g. primary key) of layer
+        csv_field: the id of the csv layer (which is always 'id' in the case
+            of the CustomCommand scripts)
+
+    Returns:
+        the generated csv layer
+    """
+    filename = os.path.basename(filepath)
+    csv_layer_name = os.path.splitext(filename)[0]
+    assert STATS_LAYER_IDENTIFIER in csv_layer_name, (
+        "Error in hardcoded stuff needed to make cache clearer work")
+    csv_uri = "file:///" + filepath
+    csv_layer = QgsVectorLayer(csv_uri, csv_layer_name, "delimitedtext")
+    QgsProject.instance().addMapLayer(csv_layer,
+                                      addToLegend=add_to_legend)
+    join_info = QgsVectorLayerJoinInfo()
+    join_info.joinLayerId = csv_layer.id()
+    join_info.joinFieldName = csv_field
+    join_info.targetFieldName = view_layer_field
+    join_info.memoryCache = True
+    layer.addJoin(join_info)
+    return csv_layer
+
+
+def _calc_results(
+        ncstats, parameters, layer_name, feature_id,
+        surface_level=None, bottom_level=None):
+    """Calcs results for all parameters and puts them in a dict (used in
+    generate_manhole_stats)
+
+    Args:
+        ncstats: NcStats instance
+        parameters: a list of (netCDF) parameters
+        layer_name: the name of the layer we want to query results for
+        feature_id: feature id (is related to the layer_name)
+        surface_level: an additional parameter that is needed for
+            calculating 'wos_height'
+        bottom_level: an additional parameter that is needed for calculing
+            'water_depth'
+
+    Note: when the last two kwargs are missing, the results cannot be
+    calculated and are simply set to None
+
+    Returns:
+        a dictionary {param_name: result_value, ...}
+    """
+    result = dict()
+    for param_name in parameters:
+        # Business as usual (NcStats method)
+        try:
+            result[param_name] = \
+                ncstats.get_value_from_parameter(
+                    layer_name, feature_id, param_name,
+                    surface_level=surface_level,
+                    bottom_level=bottom_level)
+        except (ValueError, IndexError, AttributeError):
+            result[param_name] = None
+        except TypeError:
+            # Probably an error with wos_duration, which
+            # will ONLY work for structures with a surface_level (
+            # i.e. manholes).
+            result[param_name] = None
+    return result
+
+
+def get_manhole_layer_id_name(layer_name):
+    """Get the primary key name of the layer (only node layers!)"""
+    if layer_name == 'nodes':
+        # It's a memory layer
+        layer_id_name = 'id'
+    elif 'v2' in layer_name:
+        # It's a v2 spatialite layer
+        layer_id_name = 'id'
+    else:
+        # It's sewerage spatialite (no agg. netcdf)
+        layer_id_name = 'id'
+    return layer_id_name
+
+
+def get_structure_layer_id_name(layer_name):
+    """Get the primary key name of the layer (only line/structure layers!)"""
+    if layer_name == 'flowlines':
+        layer_id_name = 'id'
+    else:
+        # It's a view
+        layer_id_name = 'ROWID'
+    return layer_id_name
+
+
+def get_pump_layer_id_name(layer_name):
+    """Get the primary key name of the layer (only pump layers!)"""
+    if layer_name == 'pumplines':
+        layer_id_name = 'id'
+    else:
+        # It's a view
+        layer_id_name = 'ROWID'
+    return layer_id_name
+
+
+def generate_manhole_stats(nds, result_dir, layer, layer_id_name,
+                           include_2d=True):
+    """Generate stats for manhole like objects and write to csv.
+
+    Args:
+        nds: NetcdfDataSource
+        result_dir: output directory
+        layer: qgis layer
+        layer_id_name: the pk of the layer
+        include_2d: include 2d features (only applicable for lines)
+
+    Returns:
+        filepath generated csv
+    """
+
+    # node-like layers for which this script works (without the 'v2_' or
+    # 'sewerage_' prefix)
+    NODE_OBJECTS = ['manhole', 'connection_node', 'node']
+
+    layer_name = layer.name()
+    if not any(s in layer_name for s in NODE_OBJECTS):
+        raise ValueError("%s is not a valid node layer" % (layer_name))
+
+    # Caution: approaching HACK territory!
+    # Motivation: This is a hack for v2_manholes. Manholes just have a
+    # foreign key to v2_connection_nodes and aren't a thing in itself.
+    # So all v2_manhole stuff should be delegated to the way
+    # v2_connection_nodes works.
+    old_layer_name = layer_name
+    if layer_name == 'v2_manhole':
+        layer_name = 'v2_connection_nodes'
+
+    # TODO: not sure if we want to make ncstats distinction based on
+    # the layer type
+    if layer_name == 'nodes':
+        try:
+            ncstats = NcStatsAgg(datasource=nds)
+        except ValueError:
+            log.error('No aggregation netcdf available for statistics')
+            ncstats = NcStats(datasource=nds)
+
+    elif 'v2' in layer_name:
+        # It's a v2 spatialite layer
+        try:
+            ncstats = NcStatsAgg(datasource=nds)
+        except ValueError:
+            log.error('No aggregation netcdf available for statistics')
+            ncstats = NcStats(datasource=nds)
+    else:
+        # It's sewerage spatialite (no agg. netcdf)
+        ncstats = NcStats(datasource=nds)
+
+    parameters = ncstats.AVAILABLE_MANHOLE_PARAMETERS
+
+    # Generate data
+    result = dict()
+    for feature in layer.getFeatures():
+
+        # skip 2d stuff
+        if not include_2d:
+            try:
+                if feature['type'] == '2d':
+                    continue
+            except KeyError:
+                pass
+
+        fid = feature[layer_id_name]
+        result[fid] = dict()
+        result[fid]['id'] = fid  # normalize layer id name
+
+        try:
+            surface_level = feature['surface_level']
+        except KeyError:
+            log.error("Feature doesn't have surface level")
+            surface_level = None
+        try:
+            bottom_level = feature['bottom_level']
+        except KeyError:
+            log.error("Feature doesn't have bottom level")
+            bottom_level = None
+
+        # There are two hacks:
+        # Hack for v2_manhole, see previous comment.
+        if old_layer_name == 'v2_manhole':
+            hack_fid = feature['connection_node_id']
+        else:
+            hack_fid = feature[layer_id_name]
+
+        results_from_params = _calc_results(
+            ncstats,
+            parameters,
+            layer_name,
+            hack_fid,
+            surface_level=surface_level,
+            bottom_level=bottom_level)
+        result[fid].update(results_from_params)
+
+    # Write to csv file
+    filepath = get_default_csv_path(old_layer_name, result_dir)
+    with open(filepath, 'w') as csvfile:
+        fieldnames = ['id'] + parameters
+
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames,
+                                delimiter=',')
+        writer.writeheader()
+        for fid, val_dict in list(result.items()):
+            writer.writerow(val_dict)
+    return filepath
+
+
+def generate_structure_stats(nds, result_dir, layer, layer_id_name,
+                             include_2d=True):
+    """Generate stats for structure objects and write to csv.
+
+    Args:
+        nds: NetcdfDataSource
+        result_dir: output directory
+        layer: qgis layer
+        layer_id_name: the pk of the layer
+        include_2d: include 2d features (only applicable for lines)
+
+    Returns:
+        filepath generated csv
+    """
+    layer_name = layer.name()
+    structures = ['weir', 'pumpstation', 'pipe', 'orifice', 'culvert',
+                  'flowline']
+    if not any(s in layer_name for s in structures):
+        raise ValueError(
+            "%s is not a valid structure layer. Valid layers are: %s" %
+            (layer_name, structures))
+
+    if layer_name == 'flowlines':
+        # TODO: not sure if we want to make ncstats distinction based on
+        # the layer type
+        try:
+            ncstats = NcStatsAgg(datasource=nds)
+        except ValueError:
+            log.error('No aggregation netcdf available for statistics')
+            ncstats = NcStats(datasource=nds)
+    else:
+        # It's a view
+        try:
+            ncstats = NcStatsAgg(datasource=nds)
+        except ValueError:
+            log.error('No aggregation netcdf available for statistics')
+            ncstats = NcStats(datasource=nds)
+
+    # Generate data
+    result = dict()
+    for feature in layer.getFeatures():
+        # skip 2d stuff
+        if not include_2d:
+            try:
+                if feature['type'] == '2d':
+                    continue
+            except KeyError:
+                pass
+
+        fid = feature[layer_id_name]
+        result[fid] = dict()
+        result[fid]['id'] = fid  # normalize layer id name to 'id' in csv
+        for param_name in ncstats.AVAILABLE_STRUCTURE_PARAMETERS:
+            try:
+                result[fid][param_name] = \
+                    ncstats.get_value_from_parameter(
+                        layer_name, feature[layer_id_name], param_name)
+            except (ValueError, IndexError, AttributeError):
+                # AttributeError: is raised in NcStats because in case of
+                # KeyErrors in get_value_from_parameter the method finding
+                # is propagated to NcStats and when the method doesn't
+                # exist AttributeError is raised. A better solution is to
+                # filter AVAILABLE_STRUCTURE_PARAMETERS beforehand.
+                result[fid][param_name] = None
+
+    # Write to csv file
+    filepath = get_default_csv_path(layer_name, result_dir)
+    with open(filepath, 'w') as csvfile:
+        fieldnames = ['id'] + ncstats.AVAILABLE_STRUCTURE_PARAMETERS
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames,
+                                delimiter=',')
+        writer.writeheader()
+        for fid, val_dict in list(result.items()):
+            writer.writerow(val_dict)
+    return filepath
+
+
+def generate_pump_stats(nds, result_dir, layer, layer_id_name,
+                        include_2d=True):
+    """Generate stats for structure objects and write to csv.
+
+    Args:
+        nds: NetcdfDataSource
+        result_dir: output directory
+        layer: qgis layer
+        layer_id_name: the pk of the layer
+        include_2d: include 2d features (only applicable for lines)
+
+    Returns:
+        filepath generated csv
+    """
+    layer_name = layer.name()
+    structures = ['pumpstation', 'pumplines']
+    if not any(s in layer_name for s in structures):
+        raise ValueError(
+            "%s is not a valid structure layer. Valid layers are: %s" %
+            (layer_name, structures))
+
+    if layer_name == 'pumplines':
+        # TODO: not sure if we want to make ncstats distinction based on
+        # the layer type
+        try:
+            ncstats = NcStatsAgg(datasource=nds)
+        except ValueError:
+            ncstats = NcStats(datasource=nds)
+    else:
+        # It's a view
+        ncstats = NcStats(datasource=nds)
+
+    # Generate data
+    result = dict()
+    for feature in layer.getFeatures():
+        # skip 2d stuff
+        if not include_2d:
+            try:
+                if feature['type'] == '2d':
+                    continue
+            except KeyError:
+                pass
+
+        fid = feature[layer_id_name]
+        result[fid] = dict()
+        result[fid]['id'] = fid  # normalize layer id name to 'id' in csv
+
+        try:
+            capacity = feature['pump_capacity']
+        except KeyError:
+            log.error("Feature doesn't have pump_capacity")
+            capacity = None
+
+        for param_name in ncstats.AVAILABLE_PUMP_PARAMETERS:
+            try:
+                result[fid][param_name] = \
+                    ncstats.get_value_from_parameter(
+                        layer_name, feature[layer_id_name], param_name,
+                        capacity_L_per_sec=capacity)
+            except (ValueError, IndexError, AttributeError):
+                # AttributeError: is raised in NcStats because in case of
+                # KeyErrors in get_value_from_parameter the method finding
+                # is propagated to NcStats and when the method doesn't
+                # exist AttributeError is raised. A better solution is to
+                # filter AVAILABLE_STRUCTURE_PARAMETERS beforehand.
+                result[fid][param_name] = None
+            except TypeError:
+                # Error with pump_duration, likely an invalid capacity
+                result[fid][param_name] = None
+
+    # Write to csv file
+    filepath = get_default_csv_path(layer_name, result_dir)
+    with open(filepath, 'w') as csvfile:
+        fieldnames = ['id'] + ncstats.AVAILABLE_PUMP_PARAMETERS
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames,
+                                delimiter=',')
+        writer.writeheader()
+        for fid, val_dict in list(result.items()):
+            writer.writerow(val_dict)
+    return filepath
