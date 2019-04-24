6857692dc6546c3500029b360aa5c8cd6a45c17d
Reinout van Rees <reinout@vanrees.org>
2019-04-24 17:29:59 +0200

    Added logging to file + cleaned up log setup + tests

--- test/test_logging.py
+++ test/test_logging.py
@@ -1,15 +1,58 @@
-import unittest
 import logging
+import mock
 from ThreeDiToolbox.utils.qlogging import setup_logging
+from ThreeDiToolbox.utils.qlogging import logfile_path
+
+logger = logging.getLogger(__name__)
 
 
-class TestLogging(unittest.TestCase):
-    def test_logging(self):
+def _cleanup_all_handlers():
+    root_logger = logging.getLogger("")
+    our_plugin_logger = logging.getLogger("ThreeDiToolbox")
+    for handler in root_logger.handlers:
+        root_logger.removeHandler(handler)
+    for handler in our_plugin_logger.handlers:
+        our_plugin_logger.removeHandler(handler)
 
-        setup_logging()
 
-        log = logging.getLogger("test")
+def test_logfile_path():
+    assert "threedi-qgis-log.txt" in logfile_path()
 
-        log.warning("test message")
 
-        self.assertEqual(1, 1)
+def test_root_logsetup():
+    _cleanup_all_handlers()
+    setup_logging()
+    root_logger = logging.getLogger("")
+    handler_names = [handler.__class__.__name__ for handler in root_logger.handlers]
+    assert "StreamHandler" in handler_names
+    assert "FileHandler" in handler_names
+    assert "QgisLogHandler" not in handler_names
+
+
+def test_plugin_logsetup():
+    _cleanup_all_handlers()
+    setup_logging()
+    our_plugin_logger = logging.getLogger("ThreeDiToolbox")
+    handler_names = [
+        handler.__class__.__name__ for handler in our_plugin_logger.handlers
+    ]
+    assert "StreamHandler" not in handler_names
+    assert "FileHandler" not in handler_names
+    assert "QgisLogHandler" in handler_names
+
+
+def test_logging_doesnt_crash():
+    _cleanup_all_handlers()
+    setup_logging(iface=mock.Mock())
+    logger.critical("Just log something")
+    logger.error("Just log something")
+    logger.warn("Just log something")
+    logger.info("Just log something")
+    logger.debug("Just log something")
+
+
+def test_write_to_file():
+    setup_logging()
+    text = "This ends up in the logfile"
+    logger.error(text)
+    assert text in open(logfile_path()).read()
--- utils/qlogging.py
+++ utils/qlogging.py
@@ -1,28 +1,40 @@
+"""Module for setting up both python and qgis logging.
+
+In our plugin code, we just want to use ``logger.info()`` and so. This ought
+to end up in a logfile we can look at for debug purposes. Ideally, customers
+can mail it to us.
+
+Log messages also ought to end up in qgis's ``QgsMessageLog`` at the bottom of
+the screen (if you've enabled it). We should set this up only for our own
+messages, btw.
+
+TODO: there probably needs to be some tweaking of log levels. Perhaps the
+verbosity ought to be made configurable.
+
+"""
 import logging
+import os
 
 from qgis.core import Qgis
+from qgis.core import QgsApplication
 from qgis.core import QgsMessageLog
 
-
-LOGGING_TO_QGIS = {
-    logging.INFO: Qgis.Info,
-    logging.WARNING: Qgis.Warning,
-    logging.CRITICAL: Qgis.Critical,
-}
+LOGFILE_NAME = "threedi-qgis-log.txt"
 
 
-class QgisHandler(logging.Handler):
+logger = logging.getLogger()
+
+
+class QgisLogHandler(logging.Handler):
+    """logging handler to get python log messages into the qgis interface."""
+
     def __init__(self, iface, *args, **kwargs):
         logging.Handler.__init__(self, *args, **kwargs)
         self.iface = iface
 
-        # add references, because this somehow does not work directly,
-        # something with the scope of the emit function (does this exists
-        # in Python?)
-        self.qgsMessageLog_ref = QgsMessageLog
-        self.logging_ref = logging
-
     def emit(self, record):
+        """Show info/warn/error in message area; critical in the messagebar.
+        """
         msg = self.format(record)
         if record.levelno >= logging.CRITICAL:
             level = Qgis.Critical
@@ -35,25 +47,40 @@ class QgisHandler(logging.Handler):
         QgsMessageLog.logMessage(msg, level=level)
 
 
+def logfile_path():
+    return os.path.join(QgsApplication.qgisSettingsDirPath(), LOGFILE_NAME)
+
+
 def setup_logging(iface=None):
+    """Set up python and qgis logging.
 
-    logger = logging.getLogger("")  # set up a root logger
+    All python logging should go to a log file. Every time we start, we start
+    the file anew.
 
-    ql = QgisHandler(iface)
-    ql.setLevel(logging.INFO)
+    It should also go to the console.
 
-    # fh = logging.FileHandler('plugin_log.logger')
-    # fh.setLevel(logging.WARNING)
+    The qgis logging inside the interface should only be what we ourselves
+    want to log, to prevent us from adding messages from other plugins
+    multiple times.
 
-    st = logging.StreamHandler()
-    st.setLevel(logging.INFO)
+    """
+    root_logger = logging.getLogger("")
+    our_plugin_logger = logging.getLogger("ThreeDiToolbox")
 
-    format = logging.Formatter("%(name)-12s - %(levelname)-8s - %(message)s")
-    # fh.setFormatter(format)
-    st.setFormatter(format)
+    log_format = logging.Formatter("%(name)s %(levelname)s %(message)s")
 
-    logger.addHandler(ql)
-    # logger.addHandler(fh)
-    logger.addHandler(st)
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(logging.INFO)
+    stream_handler.setFormatter(log_format)
+    root_logger.addHandler(stream_handler)
 
-    logger.setLevel(logging.INFO)  # set level of root logger
+    qgis_log_handler = QgisLogHandler(iface)
+    qgis_log_handler.setLevel(logging.INFO)
+    qgis_log_handler.setFormatter(log_format)
+    our_plugin_logger.addHandler(qgis_log_handler)
+
+    file_handler = logging.FileHandler(logfile_path(), mode="w", encoding="utf-8")
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(log_format)
+    root_logger.addHandler(file_handler)
+    logger.info("Started logfile: %s", logfile_path())
