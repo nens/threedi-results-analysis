cbddf752447e3faaacaf0ef8e9f4c0eea1e35a7c
Reinout van Rees <reinout@vanrees.org>
2019-04-24 13:38:45 +0200

    cleanup

--- __init__.py
+++ __init__.py
@@ -1,26 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-/***************************************************************************
- ThreeDiToolbox
-                                 A QGIS plugin for working with 3Di
-                                 hydraulic models
-                             -------------------
-        begin                : 2016-03-04
-        copyright            : (C) 2016 by Nelen&Schuurmans
-        email                : servicedesk@nelen-schuurmans.nl
-        git sha              : $Format:%H$
- ***************************************************************************/
-
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
- This script initializes the plugin, making it known to QGIS.
-"""
+"""This script initializes the plugin, making it known to QGIS."""
 import imp
 import sys
 import os
