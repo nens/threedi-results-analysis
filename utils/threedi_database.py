07d6bbcbe5a114910e115ce38d9317050f1852db
renierkramer <renier.kramer@nelen-schuurmans.nl>
2019-05-02 10:46:04 +0200

    row is feature not attribute

--- utils/threedi_database.py
+++ utils/threedi_database.py
@@ -263,7 +263,7 @@ class ThreediDatabase(object):
     def fix_spatial_index(self):
         """ fixes spatial index all tables in spatialite in multiple steps
         1.  Create new spatial indices. Each v2_ tbl must have spatial index, otherwise
-            one gets an SQL error while deleting an attribute from a table (e.g.
+            one gets an SQL error while deleting an feature (row) from a table (e.g.
             v2_2d_boundary_conditions row delete returns "no such table:
             main.idx_v2_2d_boundary_conditions_the_geom"
         2.  Make sure all spatial indices are valid
