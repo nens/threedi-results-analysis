451e75e9b579c5d53054a6cb74f2ecdc1eccdee7
richard <richard.boon@nelen-schuurmans.nl>
2019-05-07 14:39:30 +0200

    Fixed some imports which went wrong with previous refactors.

--- water_balance/tools/waterbalance.py
+++ water_balance/tools/waterbalance.py
@@ -12,7 +12,7 @@ import numpy.ma as ma
 from qgis.PyQt.QtCore import Qt
 from qgis.PyQt.QtWidgets import QMessageBox
 from qgis.core import QgsFeatureRequest, QgsPointXY
-from datasource.threedi_results import find_h5_file
+from ThreeDiToolbox.datasource.threedi_results import find_h5_file
 from ThreeDiToolbox.utils.patched_threedigrid import GridH5Admin
 
 
