5ea608ad8f513e29238fce4fb6552dba8370a4ec
richard <richard.boon@nelen-schuurmans.nl>
2019-05-07 11:32:02 +0200

    Added CUMULATIVE_AGGREGATION_UNITS which got lost during refactoring.

--- datasource/result_constants.py
+++ datasource/result_constants.py
@@ -76,6 +76,28 @@ AGGREGATION_OPTIONS = {
     "current",
 }
 
+# Explanation: aggregation using the cumulative method integrates the variable
+# over time. Therefore the units must be multiplied by the time.
+CUMULATIVE_AGGREGATION_UNITS = {
+    "s1": "m MSL",
+    "q": "m3",
+    "u1": "m",
+    "vol": "m3",
+    "q_pump": "m3",
+    "qp": "m3",
+    "up1": "m",
+    "q_lat": "m3",
+    "vol1": "m3",
+    "rain": "m3",
+    "infiltration_rate": "m3",
+    "infiltration_rate_simple": "m3",
+    "leak": "m3",
+    "su": "",
+    "au": "",
+    "intercepted_volume": "m3",
+    "q_sss": "m3",
+}
+
 layer_information = [
     # layer name, (normalized) object_type, q/h type
     # Note: the reason why this is plural is because this is (inconsistently)
