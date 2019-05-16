ee939e78b8c52b97f763a340a1cccf9ffe6269c3
renierkramer <renier.kramer@nelen-schuurmans.nl>
2019-05-16 11:58:57 +0200

    add rest to tool, still need to write tests

--- tool_result_selection/threedi_result_selection.py
+++ tool_result_selection/threedi_result_selection.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 # (c) Nelen & Schuurmans, see LICENSE.rst.
 
-from ThreeDiToolbox.models.result_downloader import DownloadResultModel
+from ThreeDiToolbox.tool_result_selection.result_downloader import DownloadResultModel
 from ThreeDiToolbox.utils.user_messages import messagebar_message
 from ThreeDiToolbox.utils.user_messages import pop_up_info
-from ThreeDiToolbox.views.result_selection import ThreeDiResultSelectionWidget
+from ThreeDiToolbox.tool_result_selection.result_selection import \
+    ThreeDiResultSelectionWidget
 from qgis.core import QgsNetworkAccessManager
 from qgis.PyQt.QtCore import pyqtSignal
 from qgis.PyQt.QtCore import QObject
