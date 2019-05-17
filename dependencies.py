32f48dd2aeb9662936c8af3b4a6d62d2ec2f8e55
Reinout van Rees <reinout@vanrees.org>
2019-05-17 13:49:58 +0200

    Fixed h5py version to match downloaded windows file

--- dependencies.py
+++ dependencies.py
@@ -41,7 +41,7 @@ Dependency = namedtuple("Dependency", ["name", "package", "constraint"])
 DEPENDENCIES = [
     Dependency("GeoAlchemy2", "geoalchemy2", ">=0.6.2, <0.7"),
     Dependency("SQLAlchemy", "sqlalchemy", ">=1.1.11, <1.2"),
-    Dependency("h5py", "h5py", ">= 2.7.1"),
+    Dependency("h5py", "h5py", ">= 2.7.0"),
     Dependency("lizard-connector", "lizard_connector", "==0.6"),
     Dependency("pyqtgraph", "pyqtgraph", ">=0.10.0"),
     Dependency("threedigrid", "threedigrid", "==1.0.13"),
