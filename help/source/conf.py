7bfc68f7544a4a4571cfadbec44bc38f3e368346
renierkramer <renier.kramer@nelen-schuurmans.nl>
2019-05-21 10:12:10 +0200

    [DOING] Renier isolate tool sideview (#376)
    * isolate tool sideview
    
    * tests, black, flake8
    
    * duplicate import
    
    * black and isort

--- help/source/conf.py
+++ help/source/conf.py
@@ -218,11 +218,5 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (
-        "index",
-        "TemplateClass",
-        "ThreeDiToolbox Documentation",
-        ["Nelen&Schuurmans"],
-        1,
-    )
+    ("index", "TemplateClass", "ThreeDiToolbox Documentation", ["Nelen&Schuurmans"], 1)
 ]
