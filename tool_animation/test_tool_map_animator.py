823eff32fc52ca774e11f81f4e863bd9c63b9439
renier.kramer <renier.kramer@nelen-schuurmans.nl>
2019-05-21 10:42:45 +0200

    delete test that does nothing

--- tool_animation/test_tool_map_animator.py
+++ tool_animation/test_tool_map_animator.py
@@ -13,11 +13,3 @@ class TestMapAnimator(unittest.TestCase):
         self.tdi_root_tool = ThreeDiToolbox(self.iface, ts_datasource)
         self.toolbar_animation = self.iface.addToolBar("ThreeDiAnimation")
         self.toolbar_animation.setObjectName("ThreeDiAnimation")
-
-    # @mock.patch("ThreeDiToolbox.tool_animation.map_animator.QWidget")
-    def test_instanciate_map_animator(self, qwidget_mock):
-        """ parent class of MapAnimator is QWidget that we mock here """
-        # TODO: first refactor animation tool so that we can test it
-        # self.map_animator_widget = MapAnimator(
-        #     self.toolbar_animation, self.iface, self.tdi_root_tool
-        # )
