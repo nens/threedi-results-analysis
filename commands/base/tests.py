1b2b02bf6d609cc4fb3252bf6cfc38047119b288
Reinout van Rees <reinout@vanrees.org>
2019-05-22 16:33:11 +0200

    black

--- commands/base/tests.py
+++ commands/base/tests.py
@@ -2,6 +2,7 @@ from ThreeDiToolbox.commands.base.custom_command import CustomCommandBase
 
 import pytest
 
+
 @pytest.mark.parametrize("method_name", ["run_it", "show_gui", "run"])
 def test_signature(method_name):
     """The base class has three methods that raise NotImplementedError."""
