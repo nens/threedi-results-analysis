111cd1ecf786926e5734fcc904e4cc5e2a56002d
Reinout van Rees <reinout@vanrees.org>
2019-05-22 15:39:16 +0200

    Removed unused load_defaults() method

--- commands/base/custom_command.py
+++ commands/base/custom_command.py
@@ -5,24 +5,6 @@ import abc
 
 
 class CustomCommandBase(with_metaclass(abc.ABCMeta, object)):
-    def load_defaults(self):
-        """If you only want to use run_it without show_gui, you can try calling
-        this method first to set some defaults.
-
-        This method will try to load the first datasource and the current QGIS
-        layer.
-        """
-        try:
-            self.datasource = self.ts_datasource.rows[0]
-        except IndexError:
-            pop_up_info("No datasource found. Aborting.", title="Error")
-            return
-
-        # Current layer information
-        self.layer = self.iface.mapCanvas().currentLayer()
-        if not self.layer:
-            pop_up_info("No layer selected, things will not go well..", title="Error")
-            return
 
     @abc.abstractmethod
     def run_it(self):
