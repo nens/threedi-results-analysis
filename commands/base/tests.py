ccb93427348909b0aa1da69b6ca59dba0c919141
Reinout van Rees <reinout@vanrees.org>
2019-05-22 16:07:09 +0200

    Added docstring

--- commands/base/tests.py
+++ commands/base/tests.py
@@ -4,6 +4,7 @@ import pytest
 
 @pytest.mark.parametrize("method_name", ["run_it", "show_gui", "run"])
 def test_signature(method_name):
+    """The base class has three methods that raise NotImplementedError."""
     sample_object = CustomCommandBase()
     with pytest.raises(NotImplementedError):
         getattr(sample_object, method_name)()
