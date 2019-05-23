f8bfd98e6af39ac5dcc66e5bc9109dfc2b8d8ed4
Reinout van Rees <reinout@vanrees.org>
2019-05-23 14:35:22 +0200

    Moved code out of a try/except's "else". More use of "continue".

--- utils/sqlalchemy_add_columns.py
+++ utils/sqlalchemy_add_columns.py
@@ -30,83 +30,75 @@ def create_and_upgrade(engine, metadata):
         except exc.NoSuchTableError:
             logger.info("Creating table %s" % model_table.name)
             model_table.create(bind=engine)
-        else:
-            ddl_c = engine.dialect.ddl_compiler(engine.dialect, None)
+            continue
 
-            logger.debug(
-                "Table %s already exists. Checking for "
-                "missing columns" % model_table.name
+        logger.debug(
+            "Table %s already exists. Checking for "
+            "missing columns" % model_table.name
+        )
+        ddl_c = engine.dialect.ddl_compiler(engine.dialect, None)
+        model_columns = _column_names(model_table)
+        db_columns = _column_names(db_table)
+
+        to_create = model_columns - db_columns
+        to_remove = db_columns - model_columns
+        to_check = db_columns.intersection(model_columns)
+
+        for c in to_create:
+            model_column = getattr(model_table.c, c)
+            logger.info("Adding column %s.%s" % (model_table.name, model_column.name))
+            assert not model_column.constraints, (
+                "I cannot automatically add columns with constraints to "
+                "the database. Please consider fixing me if you care!"
             )
+            model_col_spec = ddl_c.get_column_specification(model_column)
+            sql = "ALTER TABLE %s ADD %s" % (model_table.name, model_col_spec)
+            engine.execute(sql)
 
-            model_columns = _column_names(model_table)
-            db_columns = _column_names(db_table)
+        # It's difficult to reliably determine if the model has changed
+        # a column definition. E.g. the default precision of columns
+        # is None, which means the database decides. Therefore when I look
+        # at the model it may give the SQL for the column as INTEGER but
+        # when I look at the database I have a definite precision,
+        # therefore the returned type is INTEGER(11)
 
-            to_create = model_columns - db_columns
-            to_remove = db_columns - model_columns
-            to_check = db_columns.intersection(model_columns)
+        for c in to_check:
+            model_column = model_table.c[c]
+            db_column = db_table.c[c]
 
-            for c in to_create:
-                model_column = getattr(model_table.c, c)
-                logger.info(
-                    "Adding column %s.%s" % (model_table.name, model_column.name)
-                )
-                assert not model_column.constraints, (
-                    "I cannot automatically add columns with constraints to "
-                    "the database. Please consider fixing me if you care!"
-                )
+            logger.info("Checking column %s.%s" % (model_table.name, model_column.name))
+            try:
                 model_col_spec = ddl_c.get_column_specification(model_column)
-                sql = "ALTER TABLE %s ADD %s" % (model_table.name, model_col_spec)
-                engine.execute(sql)
-
-            # It's difficult to reliably determine if the model has changed
-            # a column definition. E.g. the default precision of columns
-            # is None, which means the database decides. Therefore when I look
-            # at the model it may give the SQL for the column as INTEGER but
-            # when I look at the database I have a definite precision,
-            # therefore the returned type is INTEGER(11)
-
-            for c in to_check:
-                model_column = model_table.c[c]
-                db_column = db_table.c[c]
-
-                logger.info(
-                    "Checking column %s.%s" % (model_table.name, model_column.name)
+                db_col_spec = ddl_c.get_column_specification(db_column)
+            except exc.CompileError:
+                logger.exception(
+                    "error in compiling %s.%s, continuing with the next column",
+                    model_table.name,
+                    model_column.name,
                 )
-                try:
-                    model_col_spec = ddl_c.get_column_specification(model_column)
-                    db_col_spec = ddl_c.get_column_specification(db_column)
-                except exc.CompileError:
-                    logger.debug(
-                        "error in compiling  %s.%s"
-                        % (model_table.name, model_column.name)
-                    )
+                continue
 
-                model_col_spec = re.sub(r"[(][\d ,]+[)]", "", model_col_spec)
-                db_col_spec = re.sub(r"[(][\d ,]+[)]", "", db_col_spec)
-                db_col_spec = db_col_spec.replace("DECIMAL", "NUMERIC")
-                db_col_spec = db_col_spec.replace("TINYINT", "BOOL")
+            model_col_spec = re.sub(r"[(][\d ,]+[)]", "", model_col_spec)
+            db_col_spec = re.sub(r"[(][\d ,]+[)]", "", db_col_spec)
+            db_col_spec = db_col_spec.replace("DECIMAL", "NUMERIC")
+            db_col_spec = db_col_spec.replace("TINYINT", "BOOL")
 
-                if model_col_spec != db_col_spec:
-                    logger.warning(
-                        "Column %s.%s has specification %r in the model "
-                        "but %r in the database"
-                        % (
-                            model_table.name,
-                            model_column.name,
-                            model_col_spec,
-                            db_col_spec,
-                        )
-                    )
-
-                if model_column.constraints or db_column.constraints:
-                    logger.debug("Column constraints not checked.")
-
-            for c in to_remove:
-                model_column = getattr(db_table.c, c)
+            if model_col_spec != db_col_spec:
                 logger.warning(
-                    "Column %s.%s in the database is not in "
-                    "the model" % (model_table.name, model_column.name)
+                    "Column %s.%s has specification %r in the model "
+                    "but %r in the database"
+                    % (model_table.name, model_column.name, model_col_spec, db_col_spec)
                 )
+
+            if model_column.constraints or db_column.constraints:
+                logger.debug("Column constraints not checked.")
+
+        for c in to_remove:
+            model_column = getattr(db_table.c, c)
+            logger.warning(
+                "Column %s.%s in the database is not in "
+                "the model" % (model_table.name, model_column.name)
+            )
 
 
 def _column_names(table):
