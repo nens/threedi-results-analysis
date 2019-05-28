c62bf97851644438b8f44859650fd80f08665948
Reinout van Rees <reinout@vanrees.org>
2019-05-28 11:04:53 +0200

    Switched over to cached-property library

--- datasource/threedi_results.py
+++ datasource/threedi_results.py
@@ -1,11 +1,11 @@
 from .base import BaseDataSource
 from .result_constants import LAYER_OBJECT_TYPE_MAPPING
 from .result_constants import SUBGRID_MAP_VARIABLES
+from cached_property import cached_property
 from threedigrid.admin.constants import NO_DATA_VALUE
 from ThreeDiToolbox.utils.patched_threedigrid import GridH5Admin
 from ThreeDiToolbox.utils.patched_threedigrid import GridH5AggregateResultAdmin
 from ThreeDiToolbox.utils.patched_threedigrid import GridH5ResultAdmin
-from ThreeDiToolbox.utils.utils import cached_property
 
 import glob
 import h5py
