269180dbba2ddb1ecafdffba97fa03981b726f7c
renier.kramer <renier.kramer@nelen-schuurmans.nl>
2019-05-29 10:09:31 +0200

    make beautiful

--- tool_commands/commands/Step 1 - Check data/raster_checker.py
+++ tool_commands/commands/Step 1 - Check data/raster_checker.py
@@ -1,10 +1,14 @@
 # (c) Nelen & Schuurmans, see LICENSE.rst.
 
 from ThreeDiToolbox.tool_commands.base.custom_command import CustomCommandBase
+from ThreeDiToolbox.tool_commands.raster_checker.raster_checker_dialog import (
+    RasterCheckerDialogWidget,
+)
+from ThreeDiToolbox.tool_commands.raster_checker.raster_checker_main import (
+    RasterChecker,
+)
 from ThreeDiToolbox.utils.threedi_database import ThreediDatabase
 from ThreeDiToolbox.utils.user_messages import messagebar_message
-from tool_commands.raster_checker.raster_checker_dialog import RasterCheckerDialogWidget
-from tool_commands.raster_checker.raster_checker_main import RasterChecker
 
 import inspect
 import logging
