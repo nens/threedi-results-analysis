02d3c6172edc2af5452ec9682dcb5de4046a22d3
Reinout van Rees <reinout@vanrees.org>
2019-06-05 15:38:53 +0200

    test coverage

--- tool_result_selection/tests.py
+++ tool_result_selection/tests.py
@@ -1,6 +1,13 @@
 from ThreeDiToolbox.tool_result_selection import log_in_dialog
+from ThreeDiToolbox.tool_result_selection import result_selection
 
 
 def test_log_in_dialog(qtbot):
     # Smoke test: just call it.
     log_in_dialog.LoginDialog()
+
+
+def test_get_valid_filename():
+    assert "johns_portrait_in_2004.jpg" == result_selection.get_valid_filename(
+        "john's portrait in 2004.jpg"
+    )
