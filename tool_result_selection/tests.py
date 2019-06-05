fe6095fab074ee079a774a0209d4bc9686d01f31
Reinout van Rees <reinout@vanrees.org>
2019-06-05 16:28:48 +0200

    Moved tests over to tests.py (in pytest form)

--- tool_result_selection/tests.py
+++ tool_result_selection/tests.py
@@ -1,5 +1,9 @@
+from ThreeDiToolbox.models.datasources import TimeseriesDatasourceModel
 from ThreeDiToolbox.tool_result_selection import log_in_dialog
 from ThreeDiToolbox.tool_result_selection import result_selection
+from ThreeDiToolbox.tool_result_selection import result_downloader
+
+import mock
 
 
 def test_log_in_dialog(qtbot):
@@ -11,3 +15,15 @@ def test_get_valid_filename():
     assert "johns_portrait_in_2004.jpg" == result_selection.get_valid_filename(
         "john's portrait in 2004.jpg"
     )
+
+
+def test_download_result_model():
+    # Smoke test, just initialize it.
+    result_downloader.DownloadResultModel()
+
+
+def test_result_selection_tool_init():
+    iface = mock.Mock()
+    ts_datasource = TimeseriesDatasourceModel()
+    result_selection_tool = result_selection.ThreeDiResultSelection(iface, ts_datasource)
+    assert "icon_add_datasource.png" in result_selection_tool.icon_path
