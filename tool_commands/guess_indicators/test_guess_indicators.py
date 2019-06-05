a92e01078317a26b97b29872674812a05a34992b
renier.kramer <renier.kramer@nelen-schuurmans.nl>
2019-06-05 14:41:15 +0200

    done test guess_manhole_indicator

--- tool_commands/guess_indicators/test_guess_indicators.py
+++ tool_commands/guess_indicators/test_guess_indicators.py
@@ -2,6 +2,7 @@ from sqlalchemy import update
 from ThreeDiToolbox.sql_models.model_schematisation import ConnectionNode
 from ThreeDiToolbox.sql_models.model_schematisation import Manhole
 from ThreeDiToolbox.sql_models.model_schematisation import Pumpstation
+from ThreeDiToolbox.sql_models.model_schematisation import BoundaryCondition1D
 from ThreeDiToolbox.test.test_init import TEST_DATA_DIR
 from ThreeDiToolbox.tool_commands.guess_indicators.guess_indicators_utils import Guesser
 from ThreeDiToolbox.utils.threedi_database import ThreediDatabase
@@ -12,20 +13,62 @@ import pytest
 from pathlib import Path
 
 
-def get_manholes_pumpstation(session):
-    """ get manholes that are on same connection node as start point of pumpstation
-    :return: a list with tuples (manhole.id, manhole.manhole_indicator)  """
+def get_pump_or_outlet_manholes(session, ids):
+    """
+    :param ids: [list] this contains ids from either manhole_outlet or manhole_pump
+    :return: a list with tuples (manhole.id, manhole.manhole_indicator)
+    """
     manholes_id_indicator = {}
-    pump_start_ids = session.query(Pumpstation.connection_node_start_id)
+    # only return manhole
     sql_manholes = (
         session.query(Manhole)
         .join(Manhole.connection_node)
-        .filter(ConnectionNode.id.in_(pump_start_ids))
+        .filter(ConnectionNode.id.in_(ids))
     )
     for manhole in sql_manholes:
         # fill dict
         manholes_id_indicator[manhole.id] = manhole.manhole_indicator
     return manholes_id_indicator
+
+
+def get_all_manholes(session):
+    """ get all manholes from sqlite (also manhole_outlet, manhole_pump)
+    :return: a list with tuples (manhole.id, manhole.manhole_indicator) """
+    manholes_id_indicator = {}
+    sql_manholes = session.query(Manhole)
+    for manhole in sql_manholes:
+        # fill dict
+        manholes_id_indicator[manhole.id] = manhole.manhole_indicator
+    return manholes_id_indicator
+
+
+def get_manholes(session, outlet_bool=False, pump_bool=False, all_bool=False):
+    """ get manholes from sqlite: OR only manhole_outlet, OR only manhole_pumps, OR
+    all manholes (manhole_outlet + manhole_pump + other manholes)
+    :return: a list with tuples (manhole.id, manhole.manhole_indicator)  """
+    # only 1 of kwargs may be True
+    assert sum([outlet_bool, pump_bool, all_bool]) == 1
+    if pump_bool:
+        ids = session.query(Pumpstation.connection_node_start_id)
+    elif outlet_bool:
+        ids = session.query(BoundaryCondition1D.connection_node_id)
+    if pump_bool or outlet_bool:
+        manholes_id_indicator = get_pump_or_outlet_manholes(session, ids)
+    if all_bool:
+        manholes_id_indicator = get_all_manholes(session)
+    return manholes_id_indicator
+
+
+def empty_manhole_selection(session, manholes_pre_empty):
+    """ empty column manhole_indicator (set is to NULL) of table v2_manhole """
+    manholes_ids = list(manholes_pre_empty)
+    up = (
+        update(Manhole)
+        .where(Manhole.id.in_(manholes_ids))
+        .values(manhole_indicator=None)
+    )
+    session.execute(up)
+    session.commit()
 
 
 @pytest.fixture()
@@ -53,47 +96,53 @@ def guesser(db):
     return Guesser(db)
 
 
-def test_manhole_indicator_pumpstation(db, guesser):
+def test_guess_manhole_indicator(db, guesser):
     session = db.get_session()
 
     # before we empty manholes, first get their [(id, manhole_indicator)]
-    manholes_pre_empty = get_manholes_pumpstation(session)
-    if not manholes_pre_empty:
+    manholes_pumps_pre_empty = get_manholes(session, pump_bool=True)
+    manholes_outlets_pre_empty = get_manholes(session, outlet_bool=True)
+    all_manholes_pre_empty = get_manholes(session, all_bool=True)
+    if not all_manholes_pre_empty:
         # skip this test as there are no manholes in sqlite..
         return
-    # now lets empty column 'manhole_indicator'
-    manholes_ids = list(manholes_pre_empty)
-    up = (
-        update(Manhole)
-        .where(Manhole.id.in_(manholes_ids))
-        .values(manhole_indicator=None)
-    )
-    session.execute(up)
-    session.commit()
+    # now lets empty column in all manholes the column 'manhole_indicator'
+    empty_manhole_selection(session, all_manholes_pre_empty)
 
-    manhole_pre_guess = get_manholes_pumpstation(session)
-    # all dict values should be None (<-- still test-prework, not actual testing)
-    assert not (all(list(manhole_pre_guess.values())))
+    all_manholes_pre_guess = get_manholes(session, all_bool=True)
+    # all dict values should be None (<-- still test-prework, not actual testing), If
+    # it fails then empty_manhole_selection() does not a good job
+    assert not (all(list(all_manholes_pre_guess.values())))
 
     # now put guesser to work
     guesser.guess_manhole_indicator(only_empty_fields=False)
 
     # get a new session
     session = db.get_session()
-    manhole_after_guess = get_manholes_pumpstation(session)
-    result_list = list(manhole_after_guess.values())
-    expected_value = Constants.MANHOLE_INDICATOR_PUMPSTATION
-    # actual test: all updated manhole_indicators should match expected_value
+
+    manholes_pumps_after_guess = get_manholes(session, pump_bool=True)
+    manholes_outlet_after_guess = get_manholes(session, outlet_bool=True)
+    all_manholes_after_guess = get_manholes(session, all_bool=True)
+
+    # manholes_pumps should have been updated to MANHOLE_INDICATOR_PUMPSTATION
+    if manholes_pumps_pre_empty:
+        result_list = list(manholes_pumps_after_guess.values())
+        expected_value = Constants.MANHOLE_INDICATOR_PUMPSTATION
+        assert all([expected_value == ele for ele in result_list])
+
+    # manholes_outlets should have been updated to MANHOLE_INDICATOR_OUTLET
+    if manholes_outlets_pre_empty:
+        result_list = list(manholes_outlet_after_guess.values())
+        expected_value = Constants.MANHOLE_INDICATOR_OUTLET
+        assert all([expected_value == ele for ele in result_list])
+
+        for k in manholes_outlets_pre_empty:
+            all_manholes_after_guess.pop(k, None)
+
+    # rest of the manholes should have been updated to MANHOLE_INDICATOR_MANHOLE
+    for my_dict in [manholes_pumps_pre_empty, manholes_outlets_pre_empty]:
+        for k in my_dict:
+            all_manholes_after_guess.pop(k, None)
+    result_list = list(all_manholes_after_guess.values())
+    expected_value = Constants.MANHOLE_INDICATOR_MANHOLE
     assert all([expected_value == ele for ele in result_list])
-
-
-# def test_manhole_indicator_outlet(db, guesser):
-#     session = db.get_session()
-#
-#     # before we empty manholes, first get their [(id, manhole_indicator)]
-#     manholes_pre_empty = get_manholes_pumpstation(session)
-#     if not manholes_pre_empty:
-#         # skip this test as there are no manholes in sqlite..
-#         return
-#
-
