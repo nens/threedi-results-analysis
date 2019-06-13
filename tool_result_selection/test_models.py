5386959f10d4375f40df7e57fbf89d4e9cad0784
Reinout van Rees <reinout@vanrees.org>
2019-06-13 14:31:22 +0200

    Added test for get_line_pattern()

--- tool_result_selection/test_models.py
+++ tool_result_selection/test_models.py
@@ -38,5 +38,32 @@ class TestTimeseriesDatasourceModel(unittest.TestCase):
 
 
 def test_downloadable_result_model():
-    # Smoke test, just initialize it.
-    models.DownloadableResultModel()
+    # Smoke test, just initialize a few.
+    downloadable_results = models.DownloadableResultModel()
+    for i in range(8):
+        test_values = {"name": "name-%s" % i,
+                    "size_mebibytes": i,
+                    "url": "http://reinout/%s" % i,
+                    "results": ""}
+        downloadable_results.insertRows([test_values])
+    assert downloadable_results.rowCount() == 8
+
+
+def test_get_line_pattern():
+    ts_datasources = models.TimeseriesDatasourceModel()
+    for i in range(8):
+        test_values = {
+            "active": False,
+            "name": "jaa",
+            "file_path": THREEDI_RESULTS_PATH,
+            "type": "netcdf-groundwater",
+            # Note: pattern is not set, we want the default.
+        }
+        ts_datasources.insertRows([test_values])
+    first_pattern = ts_datasources.rows[0].pattern.value
+    second_pattern = ts_datasources.rows[1].pattern.value
+    last_pattern = ts_datasources.rows[-1].pattern.value
+    # Different styles, if possible:
+    assert first_pattern != second_pattern
+    # If they're all used up, use the first one as fallback.
+    assert first_pattern == last_pattern
