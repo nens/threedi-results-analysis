6e8e396a2d228c948026e2b2be6bd62d4958b4f5
renier.kramer <renier.kramer@nelen-schuurmans.nl>
2019-06-14 12:36:38 +0200

    make beautiful

--- tool_commands/constants.py
+++ tool_commands/constants.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+
 COMMANDS_DIR = Path(__file__).parent
 
 modulename_packagename_mapping = {
@@ -16,10 +17,13 @@ modulename_packagename_mapping = {
 step_modulename_mapping = {
     "Step 1 - Check data": ["raster_checker.py", "schematisation_checker.py"],
     "Step 2 - Convert and import data": ["import_sufhyd.py"],
-    "Step 3 - Modify schematisation": ["control_structures.py", "guess_indicators.py",
-                                       "create_breach_locations.py",
-                                       "add_connected_points.py",
-                                       "predict_calc_points.py"],
+    "Step 3 - Modify schematisation": [
+        "control_structures.py",
+        "guess_indicators.py",
+        "create_breach_locations.py",
+        "add_connected_points.py",
+        "predict_calc_points.py",
+    ],
     "Step 4 - Convert schematisation": [],
     "Step 5 - Post-process results": [],
 }
--- tool_commands/tests.py
+++ tool_commands/tests.py
@@ -1,4 +1,5 @@
-from ThreeDiToolbox.tool_commands.command_model import CommandModel
+from ThreeDiToolbox.tool_commands.constants import COMMANDS_DIR
+from ThreeDiToolbox.tool_commands.constants import modulename_packagename_mapping
 from ThreeDiToolbox.tool_commands.custom_command_base import CustomCommandBase
 
 import pytest
@@ -12,34 +13,7 @@ def test_signature(method_name):
         getattr(sample_object, method_name)()
 
 
-def test_get_command_structure():
-    commandboxmodel = CommandModel()
-    command_structure = commandboxmodel.get_command_structure()
-    expected = {
-        "Step 1 - Check data": {
-            "raster_checker.py": None,
-            "schematisation_checker.py": None,
-        },
-        "Step 2 - Convert and import data": {"import_sufhyd.py": None},
-        "Step 3 - Modify schematisation": {
-            "control_structures.py": None,
-            "guess_indicators.py": None,
-            "create_breach_locations.py": None,
-            "add_connected_points.py": None,
-            "predict_calc_points.py": None,
-        },
-        "Step 4 - Convert schematisation": {},
-        "Step 5 - Post-process results": {},
-    }
-    assert command_structure == expected
-
-
-def test_command_modules_exists():
-    for command in COMMANDS:
-        module = COMMANDS_DIR / command.package_name / command.command_name
-        assert module.is_file()
-
-
-def test_raster_checker_can_be_called():
-    # TODO: create a test for each command and try to call run() method of command
-    pass
+def test_commands_modules_exists():
+    for modulename, packagename in modulename_packagename_mapping.items():
+        module_full_path = COMMANDS_DIR / packagename / modulename
+        assert module_full_path.is_file()
