4ac301f9b7ab7bb48eb58897e252dca0937b174c
renier.kramer <renier.kramer@nelen-schuurmans.nl>
2019-06-14 16:30:16 +0200

    dynamic import from string

--- tool_commands/command_box.py
+++ tool_commands/command_box.py
@@ -19,7 +19,7 @@
  *                                                                         *
  ***************************************************************************/
 """
-from importlib.machinery import SourceFileLoader
+import importlib
 from qgis.PyQt.QtCore import Qt
 from qgis.PyQt.QtWidgets import QAbstractItemView
 from ThreeDiToolbox.tool_commands.command_dialog_base import CommandBoxDockWidget
@@ -57,6 +57,7 @@ class CommandBox(object):
 
         self.commandboxmodel = None
         self.commandbox = None
+        self.default_command_name = "command.py"
 
     def on_unload(self):
         """Cleanup necessary items here when plugin dockwidget is closed"""
@@ -98,12 +99,24 @@ class CommandBox(object):
         """Check if QModelIndex is a leaf, i.e., has no children."""
         return q_model_index.isValid() and not q_model_index.child(0, 0).isValid()
 
-    @staticmethod
-    def get_module_path(display_name):
+    def get_module_path(self, display_name):
         package_name = display_name.replace(" ", "_")
-        default_command_name = "command.py"
-        module_path = COMMANDS_DIR / package_name / default_command_name
+        module_path = COMMANDS_DIR / package_name / self.default_command_name
         return module_path
+
+    def get_import_string(self, display_name):
+        """ return: string (e.g. 'ThreeDiToolbox.tool_commands.raster_checker.') """
+        package_name = display_name.replace(" ", "_")
+        command_without_extension = self.default_command_name.split('.py')[0]
+        import_string = __package__ + '.' + package_name + '.' + command_without_extension
+        return import_string
+
+    def import_command(self, import_string):
+        try:
+            imported_command = importlib.import_module(import_string)
+            return imported_command
+        except ImportError:
+            logging.error(f'could not import {import_string}')
 
     def run_script(self, qm_idx):
         """Dynamically import and run the selected script from the tree view.
@@ -115,21 +128,20 @@ class CommandBox(object):
 
             module_path = self.get_module_path(display_name)
             if not module_path.is_file():
-                logger.eror(f"{module_path} is not a file")
+                logger.error(f"{module_path} is not a file")
                 return
             logger.debug(f"command display name: {display_name}")
             logger.debug(f"command module_path: {module_path}")
 
-            name = module_path.stem
-            loader = SourceFileLoader(name, str(module_path))
-            mod = types.ModuleType(loader.name)
-            loader.exec_module(mod)
-            logger.debug(str(mod))
+            import_string = self.get_import_string(display_name)
+            imported_command = self.import_command(import_string)
+            if not imported_command:
+                return
 
-            command = mod.CustomCommand(
+            command_instance = imported_command.CustomCommand(
                 iface=self.iface, ts_datasources=self.ts_datasources
             )
-            command.run()
+            command_instance.run()
 
     def add_commands(self):
         self.commandboxmodel = CommandModel()
--- tool_commands/tests.py
+++ tool_commands/tests.py
@@ -12,9 +12,14 @@ def test_signature(method_name):
     with pytest.raises(NotImplementedError):
         getattr(sample_object, method_name)()
 
+import mock
+
 
 def test_command_modules_exist():
+    iface_mock = mock.MagicMock()
+    ts_datasources_mock = mock.MagicMock()
+    commandbox = CommandBox(iface_mock, ts_datasources_mock)
     for step, display_names in STEP_MODULENAME_MAPPING.items():
         for display_name in display_names:
-            module_full_path = CommandBox.get_module_path(display_name)
+            module_full_path = commandbox.get_module_path(display_name)
             assert module_full_path.is_file(), "command cannot be found"
