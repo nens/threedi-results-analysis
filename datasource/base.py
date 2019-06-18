bade451f0eda04d5542fda2613ed467d414bfcd4
Reinout van Rees <reinout@vanrees.org>
2019-06-18 12:21:51 +0200

    Zapped unused comments

--- datasource/base.py
+++ datasource/base.py
@@ -31,19 +31,3 @@ class BaseDataSource(metaclass=ABCMeta):
     @abstractmethod
     def get_values_by_timestep_nr(self, variable, timestamp_idx, index=None):
         pass  # pragma: no cover
-
-    # TODO: not sure if needed, used in creating layers
-    # @abstractmethod
-    # def channel_mapping(self):
-    #     pass
-
-    # TODO: not sure if needed, used in creating layer
-    # @abstractmethod
-    # def node_mapping(self):
-    #     pass
-
-    # TODO: used in creating layer in NetcdfDataSource, but should be
-    # refactored out because the new DataSource uses hdf5
-    # @abstractmethod
-    # def id_mapping(self):
-    #     pass
