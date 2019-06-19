cd1f7e5fe7b5134a9b5d09019ad799f11383f125
Reinout van Rees <reinout@vanrees.org>
2019-06-19 16:47:20 +0200

    Added ts_datasources fixture

--- conftest.py
+++ conftest.py
@@ -1,10 +1,10 @@
 # Pytest configuration file. The sole purpose is to prevent Qgis from grabbing
 # python's import mechanism (which breaks pytest).
 from ThreeDiToolbox import PLUGIN_DIR
-from ThreeDiToolbox.datasource.threedi_results import ThreediResult
 
 import os
 import pytest
+import shutil
 
 
 # Make pytest work in combination with qgis.
@@ -13,10 +13,7 @@ os.environ["QGIS_NO_OVERRIDE_IMPORT"] = "KEEPYOURPAWSOFF"
 
 data_dir = PLUGIN_DIR / "tests" / "data"
 bergermeer_dir = data_dir / "testmodel" / "v2_bergermeer"
-
-gridadmin_path = bergermeer_dir / "gridadmin.h5"
 results_3di_path = bergermeer_dir / "results_3di.nc"
-aggregate_results_3di_path = bergermeer_dir / "aggregate_results_3di.nc"
 
 
 @pytest.fixture()
@@ -26,4 +23,30 @@ def threedi_result():
     The instance contains result data of the model 'v2_bergermeer'. It contains
     both results and aggregate result data.
     """
+    # Late import, otherwise we get circular import errors.
+    from ThreeDiToolbox.datasource.threedi_results import ThreediResult
+
     return ThreediResult(file_path=results_3di_path)
+
+
+@pytest.fixture()
+def ts_datasources(tmp_path):
+    """Return ts_datasources with one threedi_result (the one above) preloaded.
+
+    Note that the test data is first copied, so it is safe to modify.
+
+    """
+    # Late import, otherwise we get circular import errors.
+    from ThreeDiToolbox.tool_result_selection.models import TimeseriesDatasourceModel
+
+    shutil.copytree(bergermeer_dir, tmp_path / "v2_bergermeer")
+    copied_results_3di_path = tmp_path / "v2_bergermeer" / "results_3di.nc"
+    result = TimeseriesDatasourceModel()
+    test_values = {
+        "active": False,
+        "name": "bergermeer v2 from main conftest.py",
+        "file_path": copied_results_3di_path,
+        "type": "netcdf-groundwater",
+    }
+    result.insertRows([test_values])
+    return result
