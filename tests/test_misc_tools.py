3fb137dd78a0783391219334dfd0efcb34735229
Reinout van Rees <reinout@vanrees.org>
2019-06-19 16:47:33 +0200

    Testing with copied ts_datasources so that we can actually delete something

--- tests/test_misc_tools.py
+++ tests/test_misc_tools.py
@@ -1,5 +1,4 @@
 from ThreeDiToolbox import misc_tools
-from ThreeDiToolbox.tool_result_selection.models import TimeseriesDatasourceModel
 
 import mock
 
@@ -28,12 +27,12 @@ def test_about():
     show_about_action.on_unload()  # Doesn't do anything, used for coverage.
 
 
-def test_cache_clearer():
+def test_cache_clearer(ts_datasources):
     iface = mock.Mock()
-    ts_datasources = TimeseriesDatasourceModel()
     show_cache_clearer_action = misc_tools.CacheClearer(iface, ts_datasources)
-    with mock.patch.object(misc_tools, "pop_up_info"):
-        # Only a smoke test at the moment.
-        show_cache_clearer_action.run()
+    with mock.patch.object(misc_tools, "pop_up_question") as mock_pop_up:
+        with mock.patch.object(misc_tools, "pop_up_info"):
+            mock_pop_up.return_value = True
+            show_cache_clearer_action.run()
 
     show_cache_clearer_action.on_unload()  # Doesn't do anything, used for coverage.
