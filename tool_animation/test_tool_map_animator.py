29a24feaf0345c78cd8985df26e6a2606c076e03
Reinout van Rees <reinout@vanrees.org>
2019-06-19 16:59:46 +0200

    Added TODO note that the test doesn't make sense :-)

--- tool_animation/test_tool_map_animator.py
+++ tool_animation/test_tool_map_animator.py
@@ -5,9 +5,16 @@ import mock
 
 
 def test_smoke():
-    """Test whether ThreeDiAnimation can be instantiated """
+    """Test whether ThreeDiAnimation can be instantiated.
+
+    TODO: this was the setUp() of an otherwise empty unittest class.  It
+    doesn't look like it actually tests something. Especially the
+    toolbar_animation is a mock, so calling that has no use.
+
+    """
     ts_datasources = TimeseriesDatasourceModel()
     iface = mock.Mock()
     tdi_root_tool = CommandBox(iface, ts_datasources)
     toolbar_animation = iface.addToolBar("ThreeDiAnimation")
     toolbar_animation.setObjectName("ThreeDiAnimation")
+    assert tdi_root_tool
