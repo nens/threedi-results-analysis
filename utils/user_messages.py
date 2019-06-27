22ac9fad2a98947f1b23e0096e474805781a59ca
richard <richard.boon@nelen-schuurmans.nl>
2019-06-27 10:47:18 +0200

    Make sure the progress_bar always closes, also when an exception occurs.

--- utils/user_messages.py
+++ utils/user_messages.py
@@ -99,14 +99,16 @@ def progress_bar(iface, min_value=1, max_value=100):
     # clear the message bar
     iface.messageBar().clearWidgets()
     # set a new message bar
-    progressMessageBar = iface.messageBar()
+    try:
+        progressMessageBar = iface.messageBar()
 
-    _progress_bar = QProgressBar()
-    # Maximum is set to 100, making it easy to work with
-    # percentage of completion
-    _progress_bar.setMinimum(min_value)
-    _progress_bar.setMaximum(max_value)
-    # pass the progress bar to the message Bar
-    progressMessageBar.pushWidget(_progress_bar)
-    yield _progress_bar
-    iface.messageBar().clearWidgets()
+        _progress_bar = QProgressBar()
+        # Maximum is set to 100, making it easy to work with
+        # percentage of completion
+        _progress_bar.setMinimum(min_value)
+        _progress_bar.setMaximum(max_value)
+        # pass the progress bar to the message Bar
+        progressMessageBar.pushWidget(_progress_bar)
+        yield _progress_bar
+    finally:
+        iface.messageBar().clearWidgets()
