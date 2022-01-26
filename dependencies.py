1d29332770e671e269d26b05a804392c9cd3b60c
patrick-nijman <patrick.nijman@nelen-schuurmans.nl>
2022-01-26 10:20:54 +0100

    Updated Dependecies, let's check if it works

--- dependencies.py
+++ dependencies.py
@@ -38,19 +38,18 @@ Dependency = namedtuple("Dependency", ["name", "package", "constraint"])
 
 #: List of expected dependencies.
 DEPENDENCIES = [
-    Dependency("SQLAlchemy", "sqlalchemy", ">=1.3.0, <1.4"),
-    Dependency("GeoAlchemy2", "geoalchemy2", ">=0.6.2, <0.7"),
+    Dependency("SQLAlchemy", "sqlalchemy", "<1.4"),
+    Dependency("GeoAlchemy2", "geoalchemy2", ">=0.10"),
     Dependency("lizard-connector", "lizard_connector", "==0.7.3"),
-    Dependency("pyqtgraph", "pyqtgraph", ">=0.11.1, <0.12"),
-    Dependency("threedigrid", "threedigrid", "==1.1.13"),
+    Dependency("pyqtgraph", "pyqtgraph", ">=0.12"),
+    Dependency("threedigrid", "threedigrid", ">=1.1.13"),
     Dependency("cached-property", "cached_property", ""),
-    Dependency("threedi-modelchecker", "threedi_modelchecker", ">=0.24"),
-    Dependency("threedidepth", "threedidepth", "==0.5"),
-    Dependency("click", "click", ">=7.0"),
-    Dependency("alembic", "alembic", "==1.6.5"),
+    Dependency("threedi-modelchecker", "threedi_modelchecker", ">=0.24.2"),
+    Dependency("threedidepth", "threedidepth", "==0.4"),
+    Dependency("click", "click", ">=8.0"),
+    Dependency("alembic", "alembic", ">=1.7.5"),
     Dependency("mako", "mako", ""),
     Dependency("netCDF4", "netCDF4", ""),
-    Dependency("h5netcdf", "h5netcdf", ""),
     Dependency("cftime", "cftime", ""),
 ]
 
