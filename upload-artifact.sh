c6fc8aeea6363cb986894d67538b8defcf2720ef
Ben van Basten <ben.vanbasten@nelen-schuurmans.nl>
2022-06-08 16:40:15 +0200

    Fixed deploy bug

--- upload-artifact.sh
+++ upload-artifact.sh
@@ -2,7 +2,7 @@
 set -e
 set -u
 
-VERSION=$(grep "^version" ./threedi_schematisation_editor/metadata.txt | cut -d= -f2)
+VERSION=$(grep "^version" ./threedi-results-analysis/metadata.txt | cut -d= -f2)
 
 # ARTIFACTS_KEY should be set as env variable in the travis UI.
 ARTIFACT=threedi_schematisation_editor.${VERSION}.zip
