a67d6e2fbb4a9db2cb8a64b32cb2b41fbabe62d7
Ben van Basten <ben.vanbasten@nelen-schuurmans.nl>
2022-06-20 12:01:51 +0200

    Resolved flake8 issues

--- dependencies.py
+++ dependencies.py
@@ -52,8 +52,8 @@ DEPENDENCIES = [
     Dependency("netCDF4", "netCDF4", ""),
     Dependency("cftime", "cftime", ""),
     Dependency("packaging", "packaging", ""),
-    Dependency("python-editor", "editor", ">=0.3"), #dep of alembic
-    Dependency("colorama", "colorama", ""), #dep of click and threedi-modelchecker (windows)
+    Dependency("python-editor", "editor", ">=0.3"),  # dep of alembic
+    Dependency("colorama", "colorama", ""),  # dep of click and threedi-modelchecker (windows)
 ]
 
 # Dependencies that contain compiled extensions for windows platform
