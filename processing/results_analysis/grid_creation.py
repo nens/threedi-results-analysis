3d94e5cfb2396b6221d2bab4963f4389487475ec
lukasz.debek <lukasz.debek@lutraconsulting.co.uk>
2022-06-29 17:14:29 +0200

    Moved results analysis algorithms into 3Di Toolbox.

--- processing/results_analysis/grid_creation.py
+++ processing/results_analysis/grid_creation.py
@@ -1,16 +1,3 @@
-# -*- coding: utf-8 -*-
-
-"""
-***************************************************************************
-*                                                                         *
-*   This program is free software; you can redistribute it and/or modify  *
-*   it under the terms of the GNU General Public License as published by  *
-*   the Free Software Foundation; either version 2 of the License, or     *
-*   (at your option) any later version.                                   *
-*                                                                         *
-***************************************************************************
-"""
-
 import os
 
 from qgis.PyQt.QtCore import QCoreApplication
