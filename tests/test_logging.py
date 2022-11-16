9c9b40381d20fb3c344e9e4675f0195f5b7789f7
Arjan Verkerk <arjan.verkerk@gmail.com>
2022-11-16 15:24:17 +0100

    Fix logging tests.

--- tests/test_logging.py
+++ tests/test_logging.py
@@ -1,4 +1,6 @@
-from ThreeDiToolbox.utils.qlogging import logfile_path
+from ThreeDiToolbox.utils.qlogging import ConsoleHandler
+from ThreeDiToolbox.utils.qlogging import FileHandler
+from ThreeDiToolbox.utils.qlogging import QgisHandler
 from ThreeDiToolbox.utils.qlogging import setup_logging
 
 import logging
@@ -12,7 +14,6 @@ def _cleanup_all_handlers():
 
     Note that we also zap the pytest log grabber, so don't count on that one
     to exist during these tests :-)
-
     """
     root_logger = logging.getLogger("")
     our_plugin_logger = logging.getLogger("ThreeDiToolbox")
@@ -23,41 +24,39 @@ def _cleanup_all_handlers():
 
 
 def test_logfile_path():
-    assert "threedi-qgis-log.txt" in logfile_path()
+    assert "threedi-qgis-log.txt" in FileHandler.get_filename()
 
 
 def test_loglevel():
     """Python's default log level is WARN. We want to see more."""
-    _cleanup_all_handlers()
+    # _cleanup_all_handlers()
     setup_logging()
     root_logger = logging.getLogger("")
     assert root_logger.getEffectiveLevel() == logging.DEBUG
 
 
 def test_root_logsetup():
-    _cleanup_all_handlers()
+    # _cleanup_all_handlers()
     setup_logging()
     root_logger = logging.getLogger("")
-    handler_names = [handler.__class__.__name__ for handler in root_logger.handlers]
-    assert "StreamHandler" in handler_names
-    assert "FileHandler" in handler_names
-    assert "QgisLogHandler" not in handler_names
+    handler_classes = [h.__class__ for h in root_logger.handlers]
+    assert ConsoleHandler in handler_classes
+    assert FileHandler in handler_classes
+    assert QgisHandler not in handler_classes
 
 
 def test_plugin_logsetup():
-    _cleanup_all_handlers()
+    # _cleanup_all_handlers()
     setup_logging()
     our_plugin_logger = logging.getLogger("ThreeDiToolbox")
-    handler_names = [
-        handler.__class__.__name__ for handler in our_plugin_logger.handlers
-    ]
-    assert "StreamHandler" not in handler_names
-    assert "FileHandler" not in handler_names
-    assert "QgisLogHandler" in handler_names
+    handler_classes = [h.__class__ for h in our_plugin_logger.handlers]
+    assert ConsoleHandler not in handler_classes
+    assert FileHandler not in handler_classes
+    assert QgisHandler in handler_classes
 
 
 def test_logging_doesnt_crash():
-    _cleanup_all_handlers()
+    # _cleanup_all_handlers()
     setup_logging()
     logger.critical("Just log something")
     logger.error("Just log something")
@@ -70,4 +69,4 @@ def test_write_to_file():
     setup_logging()
     text = "This ends up in the logfile"
     logger.error(text)
-    assert text in open(logfile_path()).read()
+    assert text in open(FileHandler.get_filename()).read()
--- utils/qlogging.py
+++ utils/qlogging.py
@@ -12,13 +12,13 @@ TODO: there probably needs to be some tweaking of log levels. Perhaps the
 verbosity ought to be made configurable.
 
 """
+from os.path import join
+import logging
+
 from qgis.core import Qgis
 from qgis.core import QgsApplication
 from qgis.core import QgsMessageLog
 from ThreeDiToolbox.utils import log_traceback_monkeypatch  # noqa
-
-import logging
-import os
 
 
 LOGFILE_NAME = "threedi-qgis-log.txt"
@@ -29,8 +29,36 @@ QGIS_FORMAT = "%(name)s\n%(message)s"  # Note: split over two lines.
 logger = logging.getLogger(__name__)
 
 
-class QgisLogHandler(logging.Handler):
+class ConsoleHandler(logging.StreamHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setLevel(logging.DEBUG)
+        self.setFormatter(logging.Formatter(PYTHON_FORMAT))
+
+
+class FileHandler(logging.FileHandler):
+    @staticmethod
+    def get_filename():
+        return join(QgsApplication.qgisSettingsDirPath(), LOGFILE_NAME)
+
+    def __init__(self, *args, **kwargs):
+        """
+        We place the logfile (called :py:data:`LOGFILE_NAME`) inside our qgis
+        profile's directory.
+        """
+        filename = self.__class__.get_filename()
+        super().__init__(filename, mode="w", encoding="utf-8")
+        self.setLevel(logging.DEBUG)
+        self.setFormatter(logging.Formatter(PYTHON_FORMAT))
+        logger.info("Started logfile: %s", filename)
+
+
+class QgisHandler(logging.Handler):
     """logging handler to get python log messages into the qgis MessageLog."""
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setLevel(logging.INFO)
+        self.setFormatter(logging.Formatter(QGIS_FORMAT))
 
     def emit(self, record):
         """Show log message in the message area
@@ -59,19 +87,6 @@ class QgisLogHandler(logging.Handler):
         QgsMessageLog.logMessage(msg, level=level)
 
 
-def logfile_path():
-    """Return logfile location
-
-    We place the logfile (called :py:data:`LOGFILE_NAME`) inside our qgis
-    profile's directory.
-
-    returns:
-        full path (str) to our logfile
-
-    """
-    return os.path.join(QgsApplication.qgisSettingsDirPath(), LOGFILE_NAME)
-
-
 def setup_logging():
     """
     Set up python and QGIS logging.
@@ -85,28 +100,15 @@ def setup_logging():
     # Python's default log level is WARN, but we also want to see DEBUG
     # messages.
     root_logger.setLevel(logging.DEBUG)
-    if not root_logger.handlers:
-        # console
-        stream_handler = logging.StreamHandler()
-        stream_handler.setLevel(logging.DEBUG)
-        python_formatter = logging.Formatter(PYTHON_FORMAT)
-        stream_handler.setFormatter(python_formatter)
-        root_logger.addHandler(stream_handler)
-        # file
-        file_handler = logging.FileHandler(logfile_path(), mode="w", encoding="utf-8")
-        file_handler.setLevel(logging.DEBUG)
-        file_handler.setFormatter(python_formatter)
-        root_logger.addHandler(file_handler)
-        logger.info("Started logfile: %s", logfile_path())
+    if not any(isinstance(h, ConsoleHandler) for h in root_logger.handlers):
+        root_logger.addHandler(ConsoleHandler())
+    if not any(isinstance(h, FileHandler) for h in root_logger.handlers):
+        root_logger.addHandler(FileHandler())
 
     # QGIS handler for all "__name__" loggers in the ThreeDiToolbox package
     our_plugin_logger = logging.getLogger("ThreeDiToolbox")
-    if not our_plugin_logger.handlers:
-        qgis_log_handler = QgisLogHandler()
-        qgis_log_handler.setLevel(logging.INFO)
-        qgis_formatter = logging.Formatter(QGIS_FORMAT)
-        qgis_log_handler.setFormatter(qgis_formatter)
-        our_plugin_logger.addHandler(qgis_log_handler)
+    if not any(isinstance(h, QgisHandler) for h in our_plugin_logger.handlers):  # noqa
+        our_plugin_logger.addHandler(QgisHandler())
 
     # We don't want all the "PyQt5.uic.properties DEBUG setting property text"
     # messages.
