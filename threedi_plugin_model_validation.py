70d635b40b505ba2f57c59a5520dd889efc662c5
Arjan Verkerk <arjan.verkerk@gmail.com>
2022-11-25 11:19:55 +0100

    Add check mark to results and models.

--- threedi_plugin_model_validation.py
+++ threedi_plugin_model_validation.py
@@ -1,4 +1,5 @@
 from qgis.PyQt.QtCore import QObject, pyqtSignal, pyqtSlot
+from qgis.PyQt.QtGui import QIcon
 from .threedi_plugin_model import ThreeDiGridItem, ThreeDiResultItem
 
 
@@ -11,8 +12,10 @@ class ThreeDiPluginModelValidator(QObject):
 
     @pyqtSlot(ThreeDiGridItem)
     def validate_grid(self, item: ThreeDiGridItem):
+        item.setIcon(QIcon("icons/check_mark.png"))
         self.grid_validated.emit(item, True)
 
     @pyqtSlot(ThreeDiResultItem)
     def validate_result(self, item: ThreeDiResultItem):
+        item.setIcon(QIcon("icons/check_mark.png"))
         self.result_validated.emit(item, True)
