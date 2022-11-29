3750dcc90b784cd3f2a93edf65bbbb4f20dbfa90
Ben van Basten <ben.vanbasten@nelen-schuurmans.nl>
2022-11-29 12:36:25 +0100

    Changed styling, naming and order of grid layers

--- threedi_plugin_loading.py
+++ threedi_plugin_loading.py
@@ -1,5 +1,5 @@
 import os
-from osgeo import ogr
+from collections import OrderedDict
 
 from threedigrid.admin.exporters.geopackage import GeopackageExporter
 from qgis.PyQt.QtCore import QObject, pyqtSlot, pyqtSignal
@@ -9,7 +9,13 @@ from qgis.utils import iface
 from ThreeDiToolbox.tool_result_selection import models
 from ThreeDiToolbox.utils.user_messages import StatusProgressBar, pop_up_critical
 from ThreeDiToolbox.utils.constants import TOOLBOX_QGIS_GROUP_NAME
+from ThreeDiToolbox.utils.utils import safe_join
 from ThreeDiToolbox.threedi_plugin_model import ThreeDiGridItem, ThreeDiResultItem
+
+styles_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "layer_styles", "grid")
+
+import logging
+logger = logging.getLogger(__name__)
 
 
 class ThreeDiPluginModelLoader(QObject):
@@ -109,28 +115,73 @@ class ThreeDiPluginModelLoader(QObject):
 
     @staticmethod
     def _add_layers_from_gpkg(path, item: ThreeDiGridItem) -> bool:
-        """Retrieves layers from gpk and add to project.
+        """Retrieves (a subset of the)  layers from gpk and add to project.
 
         Checks whether all layers contain the same CRS, if
-        so, sets this CRS on the project
+        so, sets this CRS on the project.
         """
 
-        gpkg_layers = [lr.GetName() for lr in ogr.Open(str(path))]
+        # Layers need to be in specific order and naming:
+        gpkg_layers = OrderedDict(
+            [
+                ("Pump (point)", "pump"),
+                ("Node", "node"),
+                ("Pump (line)", "pump_linestring"),
+                ("Flowline", "flowline"),
+                ("Cell", "cell"),
+                ("Obstacle", "obstacle"),
+            ]
+        )
+
         srs_ids = set()
-        for layer in gpkg_layers:
+        invalid_layers = []
+        empty_layers = []
+        for layer_name, table_name in gpkg_layers.items():
 
             # Using the QgsInterface function addVectorLayer shows (annoying) confirmation dialogs
             # iface.addVectorLayer(gpkg_file + "|layername=" + layer, layer, 'ogr')
-            vector_layer = QgsVectorLayer(str(path) + "|layername=" + layer, layer, "ogr")
+            vector_layer = QgsVectorLayer(str(path) + "|layername=" + table_name, layer_name, "ogr")
             if not vector_layer.isValid():
-                return False
+                invalid_layers.append(layer_name)
+                continue
 
-            # TODO: styling?
+            # only load layers that contain some features
+            if not vector_layer.featureCount():
+                empty_layers.append(layer_name)
+                continue
+
+            # apply the style
+            qml_path = safe_join(styles_dir, f"{table_name}.qml")
+            if os.path.exists(qml_path):
+                vector_layer.loadNamedStyle(qml_path)
+                # prior to QGIS 3.24, this method would show a, (annoying) message box
+                # warning when a style with the same styleName already existed. Unfortunately,
+                # QgsProviderRegistry::styleExists is not available in Python
+                if table_name not in vector_layer.listStylesInDatabase()[2]:
+                    vector_layer.saveStyleToDatabase(table_name, "", True, "")
 
             srs_ids.add(vector_layer.crs().srsid())
 
             # Won't add if already exists
             item.layer_group = ThreeDiPluginModelLoader._add_layer_to_group(vector_layer, item.text())
+
+        # Invalid layers info
+        if invalid_layers:
+            invalid_info = "\n\nThe following layers are missing or invalid:\n * " + "\n * ".join(invalid_layers) + "\n\n"
+            iface.messageBar().pushMessage(
+                "GeoPackage",
+                invalid_info,
+                Qgis.Warning,
+            )
+
+        # Empty layers info
+        if empty_layers:
+            empty_info = "\n\nThe following layers contained no feature:\n * " + "\n * ".join(empty_layers) + "\n\n"
+            iface.messageBar().pushMessage(
+                "GeoPackage",
+                empty_info,
+                Qgis.Warning,
+            )
 
         if len(srs_ids) == 1:
             srs_id = srs_ids.pop()
@@ -181,6 +232,6 @@ class ThreeDiPluginModelLoader(QObject):
 
         project = QgsProject.instance()
         project.addMapLayer(layer, addToLegend=False)
-        layer_group.insertLayer(0, layer)
+        layer_group.addLayer(layer)
 
         return layer_group
