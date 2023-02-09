f3260f2ea4b013e8ca0efe6587233f4bea105fde
Ben van Basten <ben.vanbasten@nelen-schuurmans.nl>
2023-02-09 14:17:22 +0100

    added result snap again

--- threedi_plugin_model_validation.py
+++ threedi_plugin_model_validation.py
@@ -3,7 +3,7 @@ from qgis.PyQt.QtGui import QIcon
 from pathlib import Path
 from qgis.core import Qgis
 from ThreeDiToolbox.threedi_plugin_model import ThreeDiGridItem, ThreeDiResultItem
-from ThreeDiToolbox.utils.user_messages import messagebar_message
+from ThreeDiToolbox.utils.user_messages import messagebar_message, pop_up_critical
 from ThreeDiToolbox.utils.constants import TOOLBOX_MESSAGE_TITLE
 import h5py
 from osgeo import ogr
@@ -57,22 +57,6 @@ class ThreeDiPluginModelValidator(QObject):
                 )
             return fail("Results file cannot be opened.")
 
-        # Any modern enough calc core adds a 'threedicore_version' atribute
-        if "threedicore_version" not in results_h5.attrs:
-            return fail("Result file is too old, please recalculate.")
-
-        # Check whether corresponding grid item belongs to same model
-        result_model_slug = results_h5.attrs['model_slug'].decode()
-
-        driver = ogr.GetDriverByName('GPKG')
-        package = driver.Open(str(grid_item.path), True)
-        grid_model_slug = package.GetMetadataItem('model_slug')
-
-        if grid_model_slug is None:
-            logger.warning("No model meta information in grid layer, skipping model validation.")
-        elif result_model_slug != grid_model_slug:
-            return fail("Result corresponds to different model than grid")
-
         # Try to open accompanying aggregate results file
         aggregate_results_path = result_item.path.with_name("aggregate_results_3di.nc")
         if aggregate_results_path.exists():
@@ -86,6 +70,42 @@ class ThreeDiPluginModelValidator(QObject):
                         "again."
                     )
                 return fail("Aggregate results file cannot be opened.")
+
+        # Any modern enough calc core adds a 'threedicore_version' atribute
+        if "threedicore_version" not in results_h5.attrs:
+            return fail("Result file is too old, please recalculate.")
+
+        # Check whether corresponding grid item belongs to same model
+        result_model_slug = results_h5.attrs['model_slug'].decode()
+
+        driver = ogr.GetDriverByName('GPKG')
+        package = driver.Open(str(grid_item.path), True)
+        grid_model_slug = package.GetMetadataItem('model_slug')
+
+        if grid_model_slug is None or grid_model_slug is None:
+            msg = "No model meta information in result or grid, skipping slug validation."
+            messagebar_message(TOOLBOX_MESSAGE_TITLE, msg, Qgis.Warning, 5)
+        elif result_model_slug != grid_model_slug:
+            # Really wrong grid, find a grid with the right slug, if not available, abort with pop-up
+            root_node = result_item.model().invisibleRootItem()
+            for i in range(root_node.rowCount()):
+                other_grid_item = root_node.child(i)
+
+                package = ogr.GetDriverByName('GPKG').Open(str(other_grid_item.path), True)
+                other_grid_model_slug = package.GetMetadataItem('model_slug')
+
+                if result_model_slug == other_grid_model_slug:
+                    logger.info("Found other corresponding grid with slug {other_grid_model_slug}, moving result to that grid.")
+
+                    # Remove the result from the model and start the processing sequence all over
+                    new_results_args = {"input_result_nc": result_item.path, "parent_item": other_grid_item, "text": result_item.text()}
+                    result_item.model().remove_result(result_item)
+                    return other_grid_item.model().add_result(**new_results_args)
+
+            msg = "Result corresponds to different model than loaded grids, removed from Results Manager"
+            result_item.model().remove_result(result_item)
+            pop_up_critical(msg)
+            return fail(msg)
 
         result_item.setIcon(QIcon(":images/themes/default/mIconSuccess.svg"))
         self.result_valid.emit(result_item, grid_item)
