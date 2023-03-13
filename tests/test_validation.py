8c29b35861dd49c0b388f8d7d840725ebc631882
Ben van Basten <ben.vanbasten@nelen-schuurmans.nl>
2023-03-13 13:30:39 +0100

    More tests

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -7,7 +7,7 @@ from pathlib import Path
 class TestModelValidator(unittest.TestCase):
     def setUp(self):
         self.model = ThreeDiPluginModel()
-        self.grid_item = ThreeDiGridItem(Path("c:/test/gridadmin.h5"))
+        self.grid_item = ThreeDiGridItem(Path("c:/test/gridadmin.h5"), "text")
 
     def test_creation(self):
         validator = ThreeDiPluginModelValidator(self.model)
