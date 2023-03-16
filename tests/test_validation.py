6f9e057a440a630c4c5f50ed5ed9ade4f3a8ba1f
Ben van Basten <ben.vanbasten@nelen-schuurmans.nl>
2023-03-16 11:26:18 +0100

    Added some mocked tests to validator

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -2,7 +2,7 @@ from threedi_results_analysis.threedi_plugin_model import ThreeDiPluginModel, Th
 from threedi_results_analysis.threedi_plugin_model_validation import ThreeDiPluginModelValidator
 import unittest
 from pathlib import Path
-from mock import patch, PropertyMock, MagicMock
+from mock import patch
 
 
 class TestModelValidator(unittest.TestCase):
@@ -20,9 +20,12 @@ class TestModelValidator(unittest.TestCase):
 
     @patch("threedi_results_analysis.threedi_plugin_model_validation.ThreeDiResultItem")
     @patch("threedi_results_analysis.threedi_plugin_model_validation.h5py.File")
-    def test_missing_aggregate_function(self, test_h5, test_mock):
-        test_h5.return_value = True
+    def test_missing_threedicore_function(self, test_h5, test_mock):
+        test_h5.return_value.attrs = ["no_threedicore_version"]
         test_mock.return_value.path.name = "results_3di.nc"
+
         validator = ThreeDiPluginModelValidator(self.model)
-        self.assertFalse(validator.validate_result("c:/test/results_3di.nc", self.grid_item))
-        # https://stackoverflow.com/questions/59045200/proper-way-to-return-mocked-object-using-pytest-fixture
+        with patch.object(validator, "result_invalid") as result_invalid:
+            self.assertFalse(validator.validate_result("c:/test/results_3di.nc", self.grid_item))
+            result_invalid.emit.assert_called_once()
+            result_invalid.emit.assert_called_once_with(test_mock.return_value, self.grid_item)
