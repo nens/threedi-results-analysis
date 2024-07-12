40bfa6ec4de28b223a49d995084ce78bbc25df5e
Hoan Phung <hoanphung.cpo@gmail.com>
2024-07-12 11:49:51 +0200

    check to minor version only

--- scripts/check-dependency-conflicts.py
+++ scripts/check-dependency-conflicts.py
@@ -1,5 +1,27 @@
 import sys
+import importlib.util
+from subprocess import CalledProcessError, run
 from pkg_resources import get_distribution, DistributionNotFound, RequirementParseError
+
+def install_package(package_name, version=None):
+    package_to_install = package_name if not version else f"{package_name}{version}"
+    try:
+        run([sys.executable, '-m', 'pip', 'install', package_to_install], check=True)
+    except CalledProcessError as e:
+        print(f"Error installing {package_name}: {e}")
+        sys.exit(1)
+
+def read_dependencies_file(file_path):
+    spec = importlib.util.spec_from_file_location("dependencies", file_path)
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+    return module.DEPENDENCIES
+
+def install_dependencies_from_file(file_path):
+    dependencies = read_dependencies_file(file_path)
+    for dep in dependencies:
+        if dep.name in ['threedi-modelchecker', 'threedigrid-builder']:
+            install_package(dep.package, dep.constraint)
 
 def get_dependency_version(package_name, dependency_name):
     try:
@@ -14,6 +36,9 @@ def get_dependency_version(package_name, dependency_name):
     return None
 
 def check_dependency_conflicts():
+    dependencies_file_path = 'dependencies.py'
+    install_dependencies_from_file(dependencies_file_path)
+
     modelchecker_schema_version = get_dependency_version('threedi-modelchecker', 'threedi-schema')
     gridbuilder_schema_version = get_dependency_version('threedigrid-builder', 'threedi-schema')
 
@@ -24,7 +49,10 @@ def check_dependency_conflicts():
     print(f"Model Checker Schema Version: {modelchecker_schema_version}")
     print(f"Grid Builder Schema Version: {gridbuilder_schema_version}")
 
-    if modelchecker_schema_version != gridbuilder_schema_version:
+    modelchecker_schema_minor_version = modelchecker_schema_version[:2]
+    gridbuilder_schema_minor_version = gridbuilder_schema_version[:2]
+
+    if modelchecker_schema_minor_version != gridbuilder_schema_minor_version:
         print(f"Version conflict detected: threedi-modelchecker requires threedi-schema=={modelchecker_schema_version}, but threedigrid-builder requires threedi-schema=={gridbuilder_schema_version}")
         sys.exit(1)
 
