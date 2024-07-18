e8b83a17daadc15a15f56f90f28bbce057fb46b1
leendertvanwolfswinkel <leendert.vanwolfswinkel@nelen-schuurmans.nl>
2024-07-19 00:29:28 +0200

    Remove shortHelpString

--- processing/rasters_to_netcdf_algorithm.py
+++ processing/rasters_to_netcdf_algorithm.py
@@ -120,36 +120,7 @@ class RastersToNetCDFAlgorithm(QgsProcessingAlgorithm):
         return "pre_process_sim_inputs"
 
     def shortHelpString(self):
-        return """
-                <h3>Introduction</h3>
-                <p>The elevation at which flow between 2D cells is possible (the 'exchange level'), depends on the elevation of the pixels directly adjacent to the cell edge. Obstacles in the DEM that do not cover the entire edge will therefore not stop the flow, i.e. water 'leaks' through the obstacle. This is more likely to occur if obstacles are diagonal and/or narrow compared to the computational grid size.</p>
-                <p>This processing algorithm detects such cases. Please inspect the locations where the algorithm identifies leaking obstacles and add grid refinement and/or obstacles to the schematisation to solve the issue if needed.</p>
-                <h3>Parameters</h3>
-                <h4>Gridadmin file</h4>
-                <p>HDF5-file (*.h5) containing a 3Di computational grid. Note that gridadmin files generated on the server contain exchange levels for 2D flowlines, whereas locally generated gridadmin files do not. In the latter case, the processing algorithm will analyse the DEM to obtain these values.</p>
-                <h4>Digital elevation model</h4>
-                <p>Raster of the schematisation's digital elevation model (DEM).</p>
-                <h4>Linear obstacles</h4>
-                <p>Obstacles in this layer will be used to update cell edge exchange levels, <i>in addition to</i> any obstacles already present in the gridadmin file (i.e. in files that were downloaded from the server). This input must be a vector layer with line geometry and a <i>crest_level</i> field</p>
-                <h4>Flowlines</h4>
-                <p>Can be used to limit the analysis to a specific part of the computational grid. For example, select flowlines that have a total discharge of > 10 m<sup>3</sup></p>
-                <h4>Minimum obstacle height (m)</h4>
-                <p>Only obstacles with a crest level that is significantly higher than the exchange level will be identified. 'Significantly higher' is defined as <em>crest level &gt; exchange level + minimum obstacle height</em>.</p>
-                <h4>Vertical search precision (m)</h4>
-                <p>The crest level of the identified obstacle will always be within <em>vertical search precision</em> of the actual crest level. A smaller value will yield more precise results; a higher value will make the algorithm faster to execute.</p>
-                <h3>Outputs</h3>
-                <h4>Obstacle in DEM&nbsp;</h4>
-                <p>Approximate location of the obstacle in the DEM. Its geometry is a straight line between the highest pixels of the obstacle on the cell edges. Attributes:</p>
-                <ul>
-                <li> id: id of the flowline this obstacle affects. If more than one flowline is affected, the flowline with the lowest exchange level is used. </li>
-                <li> crest_level: lowest elevation at which water can flow over the obstacle </li>
-                <li> exchange_level: lowest exchange level of the cell edge(s) that this obstacle applies to </li>
-                </ul>
-                <p>The styling is shows the difference between the crest level and the exchange level</p>
-                <h4>Obstacle on cell edge</h4>
-                <p>Suggested obstacle to add to the schematisation. In most cases, it is recommended solve any leaking obstacle issues with grid refinement, and only add obstacles if this does not solve the issue.</p>
-                <p>The styling shows the difference between the crest level and the exchange level</p>
-            """
+        return ""
 
     def name(self):
         """
