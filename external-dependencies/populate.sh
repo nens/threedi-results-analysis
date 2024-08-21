dbc23059e9ce83f1972b2258eb0f12cfe11969f6
leendertvanwolfswinkel <leendert.vanwolfswinkel@nelen-schuurmans.nl>
2024-08-21 15:04:50 +0200

    bump threedi deps for testing

--- external-dependencies/populate.sh
+++ external-dependencies/populate.sh
@@ -67,7 +67,7 @@ wget https://files.pythonhosted.org/packages/cd/84/66072ee12c3e79061f183c09a24be
 # Download windows wheels (cp39, win, amd64)
 
 wget https://files.pythonhosted.org/packages/b2/8e/83d9e3bff5c0ff7a0ec7e850c785916e616ab20d8793943f9e1d2a987fab/shapely-2.0.0-cp39-cp39-win_amd64.whl
-wget https://files.pythonhosted.org/packages/75/b7/7849ad6f25e17f72f4c7e411b36b456205db2c501ad495bfc97c1cd3b813/threedigrid_builder-1.14.4-cp39-cp39-win_amd64.whl
+wget https://files.pythonhosted.org/packages/96/63/bc1f271b291be432f4db390dc8bee6854ee1894b72816f142f57801b3f5b/threedigrid_builder-1.17.0-cp311-cp311-win_amd64.whl
 wget https://files.pythonhosted.org/packages/b3/89/1d3b78577a6b2762cb254f6ce5faec9b7c7b23052d1cdb7237273ff37d10/greenlet-2.0.2-cp39-cp39-win_amd64.whl#sha256=db1a39669102a1d8d12b57de2bb7e2ec9066a6f2b3da35ae511ff93b01b5d564
 wget https://files.pythonhosted.org/packages/5f/d6/5f59a5e5570c4414d94c6da4c97731deab832cbd14eaf23189d54a92d1e1/cftime-1.6.2-cp39-cp39-win_amd64.whl#sha256=86fe550b94525c327578a90b2e13418ca5ba6c636d5efe3edec310e631757eea
 
@@ -75,8 +75,8 @@ wget https://files.pythonhosted.org/packages/5f/d6/5f59a5e5570c4414d94c6da4c9773
 # Download linux wheels (both cp38 and cp310)
 wget https://files.pythonhosted.org/packages/06/07/0700e5e33c44bc87e19953244c29f73669cfb6f19868899170f9c7e34554/shapely-2.0.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl
 wget https://files.pythonhosted.org/packages/4e/03/f3bcb7d96aef6d56b62e2f25996f161c05f92a45d452165be2007b756e0f/shapely-2.0.0-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl
-wget https://files.pythonhosted.org/packages/0b/22/8868b2266baa2f8f9747100232807de4151fd720322ee5d5d724d6415d63/threedigrid_builder-1.14.4-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl
-wget https://files.pythonhosted.org/packages/bd/8c/db51b687ee80cf28787f7b862b62e77e096001513176252d824384d2739c/threedigrid_builder-1.14.4-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl
+wget https://files.pythonhosted.org/packages/b7/6a/828470fc4c45cb3f8f046f3b737a6a527ad7d9959916ce05c153d8d5ae60/threedigrid_builder-1.17.0-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl
+wget https://files.pythonhosted.org/packages/d4/22/08b7f0c1c28f5227e640fc5f216853b90b8ae616e72944a35e822cffc6ec/threedigrid_builder-1.17.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl
 wget https://files.pythonhosted.org/packages/6e/11/a1f1af20b6a1a8069bc75012569d030acb89fd7ef70f888b6af2f85accc6/greenlet-2.0.2-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl#sha256=d75209eed723105f9596807495d58d10b3470fa6732dd6756595e89925ce2470
 wget https://files.pythonhosted.org/packages/e1/17/d8042d82f44c08549b535bf2e7d1e87aa1863df5ed6cf1cf773eb2dfdf67/cftime-1.6.2-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl#sha256=acb294fdb80e33545ae54b4421df35c4e578708a5ffce1c00408b2294e70ecef
 wget https://files.pythonhosted.org/packages/44/51/bc9d47beee47afda1d335f05efa848dc403bd183344f03d431281518e8ab/cftime-1.5.0-cp38-cp38-manylinux_2_5_x86_64.manylinux1_x86_64.whl#sha256=7a820e16357dbdc9723b2059f7178451de626a8b2e5f80b9d91a77e3dac42133
