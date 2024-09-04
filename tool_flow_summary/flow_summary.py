16f6b6a00e24dc3315d1b6e6f264faad9bfe882c
Ben van Basten <ben.vanbasten@nelen-schuurmans.nl>
2024-09-04 10:40:57 +0200

    wip

--- tool_flow_summary/flow_summary.py
+++ tool_flow_summary/flow_summary.py
@@ -5,8 +5,13 @@ from qgis.PyQt.QtWidgets import QAbstractItemView
 from qgis.PyQt.QtWidgets import QAction
 from qgis.PyQt.QtWidgets import QDialog
 from qgis.PyQt.QtWidgets import QGridLayout
+from qgis.PyQt.QtWidgets import QHBoxLayout
+from qgis.PyQt.QtWidgets import QPushButton
+from qgis.PyQt.QtWidgets import QSizePolicy
+from qgis.PyQt.QtWidgets import QSpacerItem
 from qgis.PyQt.QtWidgets import QTableWidget
 from qgis.PyQt.QtWidgets import QTableWidgetItem
+from qgis.PyQt.QtWidgets import QWidget
 from threedi_results_analysis.threedi_plugin_model import ThreeDiGridItem
 from threedi_results_analysis.threedi_plugin_model import ThreeDiResultItem
 from threedi_results_analysis.threedi_plugin_tool import ThreeDiPluginTool
@@ -20,6 +25,8 @@ import os
 
 
 logger = logging.getLogger(__name__)
+
+INTERESTING_HEADERS = ["volume_balance", "volume_balance_of_0d_model"]
 
 
 class FlowSummaryTool(ThreeDiPluginTool):
@@ -37,7 +44,7 @@ class FlowSummaryTool(ThreeDiPluginTool):
         self.main_widget.setWindowTitle("Flow summary")
         self.main_widget.setLayout(QGridLayout())
         self.table_widget = QTableWidget(0, 1, self.main_widget)
-        self.table_widget.setHorizontalHeaderLabels(["Parameter"])
+        self.table_widget.setHorizontalHeaderLabels(["Variable"])
         self.table_widget.resizeColumnsToContents()
         self.table_widget.horizontalHeader().setStretchLastSection(True)
         self.table_widget.verticalHeader().hide()
@@ -48,7 +55,18 @@ class FlowSummaryTool(ThreeDiPluginTool):
         self.main_widget.hide()
         self.main_widget.setWindowFlags(Qt.WindowStaysOnTopHint)
 
-        # TODO: add ok button
+        # Add ok button
+        button_widget = QWidget(self.main_widget)
+        button_widget.setLayout(QHBoxLayout(button_widget))
+        spacer_item = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+        button_widget.layout().addItem(spacer_item)
+        ok_button = QPushButton("Ok", button_widget)
+        button_widget.layout().addWidget(ok_button, alignment=Qt.AlignRight)
+        self.main_widget.layout().addWidget(button_widget)
+        ok_button.clicked.connect(self.main_widget.hide)
+
+    def _format_variable(self, name: str) -> str:
+        return name
 
     def show_summary_grid(self, item: ThreeDiGridItem) -> None:
         results = []
@@ -57,12 +75,18 @@ class FlowSummaryTool(ThreeDiPluginTool):
             self.show_summary_result(result)
 
     def show_summary_result(self, item: ThreeDiResultItem) -> None:
+        self.table_widget.insertColumn(self.table_widget.columnCount())
+        header_item = QTableWidgetItem(item.text())
+
+        self.table_widget.setHorizontalHeaderItem(self.table_widget.columnCount()-1, header_item)
+        self.result_count = 0
+
         # find and parse the result files
         flow_summary_path = item.path.parent / "flow_summary.json"
         if not flow_summary_path.exists():
             logger.warning(f"Flow summary file from Result {item.text()} cannot be found.")
-
-        self.result_count = 0
+            # TODO: make red, but unclear how to style individual items in header
+            return
 
         # TODO: keep track of existing params
 
@@ -71,12 +95,9 @@ class FlowSummaryTool(ThreeDiPluginTool):
         # retrieve all the entries in this file
         with flow_summary_path.open() as file:
             data = json.load(file)
-            interesting_headers = ["volume_balance", "volume_balance_of_0d_model"]
-            self.table_widget.insertColumn(self.table_widget.columnCount())
-            self.table_widget.setHorizontalHeaderItem(self.table_widget.columnCount()-1, QTableWidgetItem(item.text()))
 
             row_count = 0
-            for interesting_header in interesting_headers:
+            for interesting_header in INTERESTING_HEADERS:
                 if interesting_header in data:
                     for param in data[interesting_header]:
                         self.table_widget.insertRow(self.table_widget.rowCount())
@@ -86,6 +107,8 @@ class FlowSummaryTool(ThreeDiPluginTool):
 
         self.table_widget.resizeColumnsToContents()
         self.result_count += 1
+
+        self.main_widget.show()
 
     def get_custom_actions(self) -> Dict[QAction, Tuple[Callable[[ThreeDiGridItem], None], Callable[[ThreeDiResultItem], None]]]:
         return {QAction("Show flow summary"): (self.show_summary_grid, self.show_summary_result)}
