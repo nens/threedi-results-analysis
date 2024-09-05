ed46dbf7912e20f52f98b0629a29f06a31f96663
Ben van Basten <ben.vanbasten@nelen-schuurmans.nl>
2024-09-05 15:30:47 +0200

    test

---
+++ tool_flow_summary/test_tool_flow_summary.py
@@ -0,0 +1,16 @@
+from threedi_results_analysis.tool_flow_summary.flow_summary import FlowSummaryTool
+
+import mock
+import unittest
+
+
+class TestFlowSummaryTool(unittest.TestCase):
+    def setUp(self):
+        """test whether FlowSummaryTool can be instantiated"""
+        iface = mock.Mock()
+        self.flow_summary = FlowSummaryTool(None, iface, None)
+
+    def test_icon_path_is_set(self):
+        self.assertEqual(
+            self.flow_summary.icon_path, "/root/.local/share/QGIS/QGIS3/profiles/default/python/plugins/threedi_results_analysis/icons/icon_summary.png"
+        )
