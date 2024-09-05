1b26d03c79c338113fb5baeafd7512d9b76d7732
Ben van Basten <ben.vanbasten@nelen-schuurmans.nl>
2024-09-05 17:42:17 +0200

    test

--- tool_flow_summary/test_tool_flow_summary.py
+++ tool_flow_summary/test_tool_flow_summary.py
@@ -1,7 +1,61 @@
 from threedi_results_analysis.tool_flow_summary.flow_summary import FlowSummaryTool
+from threedi_results_analysis.tool_flow_summary.flow_summary import VariableTable
 
 import mock
 import unittest
+
+
+TEST_DATA = {
+        "calculation_node_with_max_volume_error": 13973,
+        "default_timestep": {
+            "units": "s",
+            "value": 5.0146
+        },
+        "maximum_timestep": {
+            "units": "s",
+            "value": 5.0379
+        },
+        "minimum_timestep": {
+            "units": "s",
+            "value": 5.0
+        },
+        "model_id": 65899,
+        "model_name": "ilan2023-16",
+        "model_type": "1D/2D",
+        "revision_id": 58400,
+        "schematisation_id": 6745,
+        "simulation_id": 217430,
+        "simulation_start": "2024-09-03 12:00:00",
+        "simulation_time": {
+            "units": "s",
+            "value": 14400.1904
+        }
+    }
+
+TEST_DATA2 = {
+        "calculation_node_with_max_volume_error": 13973,
+        "default_timestep": {
+            "units": "s",
+            "value": 5.0146
+        },
+        "additional": 13973,
+        "additional2": {
+            "units": "s",
+            "value": 5.0146
+        },
+    }
+
+TEST_DATA3 = {
+        "additional": 2,
+        "additional2": {
+            "units": "s",
+            "value": 3
+        },
+        "simulation_time": {
+            "units": "s",
+            "value": 3.14
+        }
+    }
 
 
 class TestFlowSummaryTool(unittest.TestCase):
@@ -15,5 +69,23 @@ class TestFlowSummaryTool(unittest.TestCase):
             self.flow_summary.icon_path, "/root/.local/share/QGIS/QGIS3/profiles/default/python/plugins/threedi_results_analysis/icons/icon_summary.png"
         )
 
-    def test_result_addition(self):
-        pass
+    def test_result_addition_removal(self):
+        table = VariableTable(None)
+        assert table.columnCount() == 1
+        assert table.rowCount() == 0
+
+        table.add_summary_results("test", TEST_DATA)
+        assert table.columnCount() == 2
+        assert table.rowCount() == 12
+
+        table.add_summary_results("test", TEST_DATA2)
+        assert table.columnCount() == 3
+        assert table.rowCount() == 14
+
+        table.add_summary_results("test", TEST_DATA3)
+        assert table.columnCount() == 4
+        assert table.rowCount() == 14
+
+        table.remove_result(1)
+        assert table.columnCount() == 3
+        assert table.rowCount() == 14
