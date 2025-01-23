fed6494e764e1695e334863afb91a5ce1a54a201
Hoan Phung <hoanphung.cpo@gmail.com>
2025-01-23 12:08:19 +0100

    Fix issue with wrong version of h5py being used

--- dependencies.py
+++ dependencies.py
@@ -87,14 +87,14 @@ if QGIS_VERSION < 32806 and platform.system() == "Windows":
     H5PY_DEPENDENCY = Dependency("h5py", "h5py", "==2.10.0", False)
 elif QGIS_VERSION >= 34000 and platform.system() == "Windows":
     SUPPORTED_HDF5_VERSIONS = ["1.14.0"]
-    H5PY_DEPENDENCY = Dependency("h5py", "h5py", "==3.10.0", False)
+    H5PY_DEPENDENCY = Dependency("h5py", "h5py", "==3.10.0", True)
 else:
     SUPPORTED_HDF5_VERSIONS = ["1.14.0"]
     H5PY_DEPENDENCY = Dependency("h5py", "h5py", "==3.8.0", True)
 
 if QGIS_VERSION < 32811 and platform.system() == "Windows":
     WINDOWS_PLATFORM_DEPENDENCIES = [
-        Dependency("scipy", "scipy", "==1.6.2", True),
+        Dependency("scipy", "scipy", "==1.6.2", False),
     ]
 elif QGIS_VERSION >= 34000 and platform.system() == "Windows":
     WINDOWS_PLATFORM_DEPENDENCIES = [
@@ -102,7 +102,7 @@ elif QGIS_VERSION >= 34000 and platform.system() == "Windows":
     ]
 else:
     WINDOWS_PLATFORM_DEPENDENCIES = [
-        Dependency("scipy", "scipy", "==1.10.1", False),
+        Dependency("scipy", "scipy", "==1.10.1", True),
     ]
 
 # If you add a dependency, also adjust external-dependencies/populate.sh
