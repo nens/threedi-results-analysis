dbf96b18394472b82c930838178399f12d89dace
Ben van Basten <ben.vanbasten@nelen-schuurmans.nl>
2025-04-17 17:55:43 +0200

    wip

--- tool_fraction_analysis/fraction_model.py
+++ tool_fraction_analysis/fraction_model.py
@@ -1,6 +1,9 @@
+from qgis.PyQt.QtCore import Qt
+from qgis.PyQt.QtGui import QColor
 from qgis.PyQt.QtGui import QStandardItem
 from qgis.PyQt.QtGui import QStandardItemModel
 from threedi_results_analysis.threedi_plugin_model import ThreeDiResultItem
+from threedi_results_analysis.utils.color import COLOR_LIST
 
 import logging
 import numpy as np
@@ -24,21 +27,31 @@ class FractionModel(QStandardItemModel):
         self.result_item = item
 
         # Retrieve the substances (TODO: filter on right unit)
-        self.appendRow([QStandardItem(True), QStandardItem(), QStandardItem(item.text()), QStandardItem(str(id))])
+        threedi_result = self.result_item.threedi_result
+        water_quality_vars = threedi_result.available_water_quality_vars
+        for substance in water_quality_vars:
+            color = self.get_color()
+            logger.error("----")
+            logger.error(color)
+            color_item = QStandardItem()
+            color_item.setData((Qt.SolidLine, color))
+            self.appendRow([QStandardItem(True), color_item, QStandardItem(substance["parameters"]), QStandardItem(str(id))])
+
+    def get_color(self) -> QColor:
+        return COLOR_LIST[self.rowCount() % len(COLOR_LIST)]
 
     def create_plots(self, index, time_units, substance_unit, stacked):
         threedi_result = self.result_item.threedi_result
-        id = int(self.item(index, 3).text())
-
-        # iterate over substances        
         water_quality_vars = threedi_result.available_water_quality_vars
 
+        id = int(self.item(index, 3).text())
+        style, color = self.item(index, 1).data()
         plots = []
         for substance in water_quality_vars:
             ts_table = self.timeseries_table(substance["parameters"], id, time_units=time_units)
-            pen = pg.mkPen(color=self.color.value, width=2, style=self.result.value._pattern)
-            plots += pg.PlotDataItem(ts_table, pen=pen)
-        
+            pen = pg.mkPen(color=QColor(*color), width=2, style=style)
+            plots.append(pg.PlotDataItem(ts_table, pen=pen))
+
         return plots
     
     def timeseries_table(self, substance, id, time_units):
