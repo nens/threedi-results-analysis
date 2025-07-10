da634bf9263fa24e049fe4bb4c50dd84d63b7f23
elisalle <eli.salle@nelen-schuurmans.nl>
2025-07-10 15:59:17 +0200

    use random colour for timeseries graph when colours list is exhausted

--- tool_graph/graph_model.py
+++ tool_graph/graph_model.py
@@ -44,6 +44,7 @@ class LocationTimeseriesModel(BaseModel):
     """Model implementation for (selected objects) for display in graph"""
 
     feature_color_map = {}
+    colors = COLOR_LIST.copy()
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -53,10 +54,17 @@ class LocationTimeseriesModel(BaseModel):
         if not self.feature_color_map:
             self.feature_color_map[key] = 0
         elif key not in self.feature_color_map:
-            # pick next color from COLOR_LIST
-            self.feature_color_map[key] = ((max(self.feature_color_map.values())+1) % len(COLOR_LIST))
+            current_color = max(self.feature_color_map.values())
+            # if the list of colors is exhausted append a new random one
+            if current_color + 1 == len(self.colors):
+                new_random_color = (randint(0, 256), randint(0, 256), randint(0, 256))
+                self.colors.append(new_random_color)
+            # choose the next color in the list
+            self.feature_color_map[key] = current_color + 1
+            return self.colors[self.feature_color_map[key]]
 
-        return COLOR_LIST[self.feature_color_map[key]]
+
+        return self.colors[self.feature_color_map[key]]
 
     def flags(self, index):
 
